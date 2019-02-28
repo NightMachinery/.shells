0a666e7faac9bf5f13b1908cb36fe3613b910ba1
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2019-02-28 06:36:28 -0600

    .

--- scripts/launchers/bbbWeb.py
+++ scripts/launchers/bbbWeb.py
@@ -3,6 +3,6 @@ import random
 import pexpect
 
 if random.random() < 0.5:
-    print(pexpect.run("tecast.py 'https://t.me/joinchat/C6Vw2UhtHonAKvbWfDd30w' --log-chat 'Orphicality' --limit 1 --additional-receivers 'AliceNews Csmathlibrary '").decode())
+    print(pexpect.run("tecast.py 'https://t.me/joinchat/C6Vw2UhtHonAKvbWfDd30w' --log-chat 'Orphicality' --limit 1 --additional-receivers 'AliceNews Csmathlibrary meiotumultuado'").decode())
 else:
     print("Not this time ;-'")
