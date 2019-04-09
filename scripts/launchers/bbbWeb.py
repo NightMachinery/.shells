31df44e3d268d7d7a93d9d6252afef00a46c6ae9
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2019-04-09 21:34:47 +0430

    .

--- scripts/launchers/bbbWeb.py
+++ scripts/launchers/bbbWeb.py
@@ -2,7 +2,7 @@
 import random
 import pexpect
 
-if random.random() < 0.5:
-    print(pexpect.run("tecast.py 'https://t.me/joinchat/C6Vw2UhtHonAKvbWfDd30w' --log-chat 'Orphicality' --limit 1 --additional-receivers 'AliceNews Csmathlibrary meiotumultuado'").decode())
+if random.random() < 0.6:
+    print(pexpect.run("tecast.py 'https://t.me/joinchat/C6Vw2UhtHonAKvbWfDd30w' --log-chat 'Orphicality' --limit 1 --additional-receivers 'AliceNews math_sut_refs meiotumultuado marinavondrich Dr_Raeesi murakamitype +966596247086 N_Dhghn  '").decode())
 else:
     print("Not this time ;-'")
