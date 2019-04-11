adb65057da9dc8414729a112ce4a3ae0aa6395fe
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2019-04-12 02:56:21 +0430

    .

--- scripts/launchers/launch-rss.zsh
+++ scripts/launchers/launch-rss.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
+tmux new -d -s rss-stratechery "rss-tsend.zsh 'http://stratechery.com/feed/' '*' Orphicality"
 tmux new -d -s rss-lnbastion "rss-tsend.zsh 'https://www.lightnovelbastion.com/rss.php' 'Death Mage' Orphicality"
 tmux new -d -s rss-wuxia "rss-tsend.zsh 'https://www.wuxiaworld.com/feed/chapters' 'Renegade Immortal' Orphicality"
