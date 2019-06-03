d35972449b6017eccea0417137ab4f15c499148a
Polina <wishmerain@outlook.com>
2019-06-03 15:48:47 +0430

    .

--- scripts/zsh/auto-load/config.zsh
+++ scripts/zsh/auto-load/config.zsh
@@ -1,2 +1,9 @@
 re "silence unalias" map p fd
 typeset -U path #Makes the path array's elements unique. It needs to be run again to fix a bad PATH, or some other array operation needs to be performed.
+precmd_pipestatus() {
+	RPROMPT="${(j.|.)pipestatus}"
+       if [[ ${(j.|.)pipestatus} = 0 ]]; then
+              RPROMPT=""
+       fi
+}
+add-zsh-hook precmd precmd_pipestatus
