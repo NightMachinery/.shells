c02587eb6f63df2a98086a2c37e9eb6ad7834b76
Polina <wishmerain@outlook.com>
2019-07-04 16:14:38 +0430

    .

--- scripts/bash/auto-load/env.bash
+++ scripts/bash/auto-load/env.bash
@@ -2,3 +2,4 @@ export BROWSER="lynx"
 eve="23.227.196.103"
 export LESS=-Ri #R: maintain the ANSI colour sequences; i: smartcase searches (all lower=ignore case);
 export HOMEBREW_AUTO_UPDATE_SECS=259200 #72 hours
+export LC_ALL="en_US.UTF-8" #set locale; use sudo locale-gen to create them. update-locale is supposed to be the way to go about this, but it didn't work for me.
