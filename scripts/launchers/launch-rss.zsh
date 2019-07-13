e531f3ecfb1890007742b877f9e40682b8954b80
Polina <wishmerain@outlook.com>
2019-07-13 21:47:32 +0430

    .

--- scripts/launchers/launch-rss.zsh
+++ scripts/launchers/launch-rss.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 tmux new -d -s rss-mol "rss-tsend.zsh 'https://www.royalroad.com/syndication/21220' '.*' Orphicality"
 tmux new -d -s rss-stratechery "rss-tsend.zsh 'http://stratechery.com/feed/' '.*' Orphicality"
-tmux new -d -s rss-lnbastion "rss-tsend.zsh 'https://www.lightnovelbastion.com/rss.php' 'Death Mage' Orphicality"
+#tmux new -d -s rss-lnbastion "rss-tsend.zsh 'https://www.lightnovelbastion.com/rss.php' 'Death Mage' Orphicality"
 tmux new -d -s rss-wuxia "rss-tsend.zsh 'https://www.wuxiaworld.com/feed/chapters' 'Renegade Immortal' Orphicality"
