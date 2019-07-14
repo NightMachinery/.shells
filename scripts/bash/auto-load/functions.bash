49e7d7e5e744ca3c27129a368355f89bd9598f79
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-07-14 22:02:04 +0430

    .

--- scripts/bash/auto-load/functions.bash
+++ scripts/bash/auto-load/functions.bash
@@ -59,12 +59,16 @@ function retry-eval() {
 }
 function retry-limited() {
     local limit=0
-	  until {test "$1" -gt 0 && test $limit -ge "$1"} || eval "${@:2:q}"  ; do
+    local ecode=0
+	  until {test "$1" -gt 0 && test $limit -ge "$1"} || { eval "${@:2:q}" && ecode=0 }
+    do
+        ecode="$?"
 		    ecerr Tried "${@:2:q}" "..."
 		    sleep 1
         limit=$((limit+1))
 	  done
-    test $limit -lt "$1" || test "$1" -eq 0
+    # test $limit -lt "$1" || test "$1" -eq 0
+    (exit "$ecode")
 }
 function 2mobi() {
 	ebook-convert "$1" "${1:r}.mobi"
