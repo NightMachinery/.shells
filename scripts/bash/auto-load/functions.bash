c677bf8d6328adc40f9d533c7d1b894cbee243a1
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-07-15 15:40:36 +0430

    .

--- scripts/bash/auto-load/functions.bash
+++ scripts/bash/auto-load/functions.bash
@@ -330,7 +330,7 @@ emn() {
     emc -e "(woman \"$*\")"
 }
 swap-audio() {
-    ffmpeg -i "$1" -i "$2" -c:v copy -map 0:v:0 -map 1:a:0 "$3"
+    ffmpeg -i "$1" -i "$2" -c:v copy -map 0:v:0 -map 1:a:0 -shortest "$3"
 }
 
 function vdsox() {
