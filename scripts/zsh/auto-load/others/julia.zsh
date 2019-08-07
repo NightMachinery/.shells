cd12ccbab126700821d227e2ba12707176b560dd
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-08-07 18:07:54 +0430

    .

---
+++ scripts/zsh/auto-load/others/julia.zsh
@@ -0,0 +1,14 @@
+function jsummon() {
+    mkdir -p ~/julia_tmp/
+    local u=(*)
+    mv "$u" ~/julia_tmp/
+    realpath ~/julia_tmp/"$u"
+}
+function junsummon() {
+    \rm -r ~/julia_tmp
+}
+jdlc() {
+    cd ~/Download/
+    onlc get-dl-link
+    cd -
+}
