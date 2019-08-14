8ea4574f4258201f46694190d5ec0bdf1ae23961
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-08-14 12:05:45 +0430

    .

--- scripts/python/url-encode/url-encode.py
+++ scripts/python/url-encode/url-encode.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import fileinput
 import urllib.parse
+# import request_uri
 
 for line in fileinput.input():
-    print(urllib.parse.quote(line))
+    # print(request_uri(line))
+    print(urllib.parse.quote(line, safe='/:?=&'))
