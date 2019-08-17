67d320c968906ba84dc6be49cf2fd1ed57d11394
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-08-17 23:34:14 +0430

    .

--- scripts/zsh/auto-load/others/audiobooks.zsh
+++ scripts/zsh/auto-load/others/audiobooks.zsh
@@ -10,10 +10,13 @@ Joins the audio files in <dir>, removes the originals, generates a podcast feed,
     ecdbg Audio joined with $?
     local covers=($abdir/*cover*.(jpg|png))
     (( ${#covers} )) || {
-        fetch-ebook-metadata --title "$out" --cover $abdir/cover.png
+        silent fetch-ebook-metadata --title "$out" --timeout 120 --cover $abdir/cover.png
         covers=($abdir/*cover*.(jpg|png))
     }
-    genRSS.py -d $abdir -e "${aj_out:e}" -t "$out" -p "The night is fair ..." -o $abdir/feed.rss --image "$covers[1]" --host 'http://lilf.ir:8080'
+    (( ${#covers} )) && {
+        covers=( --image $covers[1] )
+        } 
+    genRSS.py -d $abdir -e "${aj_out:e}" -t "$out" -p "The night is fair ..." -o $abdir/feed.rss $covers[@] --host 'http://lilf.ir:8080'
     # \rm -r $tmp #URGENTTD
     get-dl-link $abdir/feed.rss
 }
