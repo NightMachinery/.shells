df0f4e6f904181881ab5d3e60e4d7219e2905c90
Eva Riin <rudiwillalwaysloveyou@gmail.com>
2020-02-28 12:42:44 -0800

    .

--- scripts/launchers/launch-rss.zsh
+++ scripts/launchers/launch-rss.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-tmux new -d -s rss-mol "rss-tsend.zsh 'https://www.royalroad.com/syndication/21220' '.*' Orphicality"
+#tmux new -d -s rss-mol "rss-tsend.zsh 'https://www.royalroad.com/syndication/21220' '.*' Orphicality"
 tmux new -d -s rss-stratechery "rss-tsend.zsh 'http://stratechery.com/feed/' '.*' Orphicality"
 tmux new -d -s rss-novelupdates "rss-tsend.zsh 'https://www.novelupdates.com/rss.php?uid=145566&unq=5c39d8aba43cc&type=0&lid=local' '.*' Orphicality"
-tmux new -d -s rss-wuxia "rss-tsend.zsh 'https://www.wuxiaworld.com/feed/chapters' 'Renegade Immortal' Orphicality"
+#tmux new -d -s rss-wuxia "rss-tsend.zsh 'https://www.wuxiaworld.com/feed/chapters' 'Renegade Immortal' Orphicality"
