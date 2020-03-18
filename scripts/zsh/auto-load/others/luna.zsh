be118e6f243a0c67d8331af660a4e0256782b61f
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2020-03-18 15:11:54 +0330

    .

--- scripts/zsh/auto-load/others/luna.zsh
+++ scripts/zsh/auto-load/others/luna.zsh
@@ -1,4 +1,4 @@
-alias lunar="lo_s=$((60*${1:-36})) lo_noinit=y lo_p=${2:-~/tmp/.luna} loop"
+alias lunar='lo_s=$((60*${1:-36})) lo_noinit=y lo_p=${2:-~/tmp/.luna} loop'
 luna() {
     lunar pmset displaysleepnow
 }
