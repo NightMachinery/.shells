960156a7f86c0328c5a4358a1b328b35d5d9bde6
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2020-03-29 19:31:54 +0430

    .

--- scripts/python/getlinks.py
+++ scripts/python/getlinks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from IPython import embed
 from bs4 import BeautifulSoup, SoupStrainer
 import requests, sys
 from urllib.parse import urlparse
@@ -17,12 +18,20 @@ soup = BeautifulSoup(data, features='lxml')
 parts = urlparse(url)
 
 page_links = []
-for link in [h.get('href') for h in soup.find_all('a')]:
-            if link.startswith('http'):
-                page_links.append(link)
-            elif link.startswith('/'):
-                page_links.append(parts.scheme + '://' + parts.netloc + link)
-            else:
-                page_links.append(url + link)
+#embed()
+sel = soup
+gl_s = local.env.get('gl_s', '')
+if gl_s != '':
+	sel = soup.select_one(gl_s)
+# We can use select to get an array of matches
+for link in [h.get('href') for h in sel.find_all('a')]:
+    if link is None:
+        continue
+    if link.startswith('http'):
+        page_links.append(link)
+    elif link.startswith('/'):
+        page_links.append(parts.scheme + '://' + parts.netloc + link)
+    else:
+        page_links.append(url + link)
 for link in page_links:
     print(link)
--- scripts/zsh/interactive/completions.zsh
+++ scripts/zsh/interactive/completions.zsh
@@ -1,4 +1,4 @@
-rexa "compdef _=which" cee ceer wh whh whz lesh mn emn fmn rp
+rexa "compdef _=which" cee ceer wh whh whz lesh mn emn fmn rp p
 rexa "compdef _=ls" pbcopy
 isExpensive && {
     [[ -e $asdf_dir ]] && . $asdf_dir/etc/bash_completion.d/asdf.bash
