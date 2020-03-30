6452148776ee2acac4017ee6be3692e05b1fc2f9
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-03-31 03:10:28 +0430

    .

---
+++ scripts/python/jwiki.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import traceback
+import wikipedia
+import sys
+from IPython import embed
+
+keywords = sys.argv[1]
+results = 3
+if len(sys.argv) >= 3:
+    results = int(sys.argv[2])
+res = wikipedia.search(keywords, results=results, suggestion=True)
+# embed()
+c = res[0]
+if len(res) == 2:
+    c.append(res[1])
+for candidate in c:
+    try:
+        p = wikipedia.page(candidate)
+        print(p.title)
+        print()
+        print(p.summary)
+        print()
+        print(p.url)
+        print()
+    except wikipedia.exceptions.DisambiguationError:
+        pass
+    except:
+        print(traceback.format_exc(), file=sys.stderr)
