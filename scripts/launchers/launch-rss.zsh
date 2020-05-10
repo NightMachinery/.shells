c4695ac00db0a0aaa360d18f8efcf3b89fa8d1b2
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-05-11 03:09:51 +0430

    .

--- scripts/launchers/launch-rss.zsh
+++ scripts/launchers/launch-rss.zsh
@@ -5,6 +5,7 @@ water='-1001293952668'
 #tmuxnew rss-mol "rss-tsend.zsh 'https://www.royalroad.com/syndication/21220' '.*' Arstar"
 tmuxnewshenv="rt_e=(tl -p 'stratechery | ')" tmuxnewsh rss-stratechery rss-tsend 'http://stratechery.com/feed/'
 tmuxnewshenv="rt_e=(tl -p 'Paul Graham | ')" tmuxnewsh rss-paul rss-tsend 'http://www.aaronsw.com/2002/feeds/pgessays.rss'
+tmuxnewsh rss-royalroad rss-tsend 'https://www.royalroad.com/syndication/21322/'
 tmuxnewshenv="rt_e=true rt_id=$arista" tmuxnewsh rss-hn rss-tsend 'http://hnapp.com/rss?q=score%3E500' 'https://lobste.rs/top/rss'
 tmuxnewshenv='rt_e=tlrlu' tmuxnewsh rss-novelupdates rss-tsend 'https://www.novelupdates.com/rss.php?uid=145566&unq=5c39d8aba43cc&type=0&lid=local'
 # tmuxnewshenv="rt_c=(rss-ctitle) rc_t='Renegade Immortal' " tmuxnewsh rss-wuxia 'https://www.wuxiaworld.com/feed/chapters'
