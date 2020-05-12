700cdd316a810ddb102f6e91cb7c7c874510404f
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-05-13 02:33:46 +0430

    .

--- scripts/launchers/various.zsh
+++ scripts/launchers/various.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-tmuxnew serve-dl 'cd ~/Downloads && http-server'
+tmuxnew serve-dl 'cd ~/Downloads && miniserve -- .' #http-server
 #tmuxnew splash 'docker run -it -p 8050:8050 scrapinghub/splash'
 borgpath=~/code/betterborg/stdborg.py
 tmuxnew julia "zsh -c 'cdd $(gq $borgpath) && python3 $(gq $borgpath)'"
