2fbd3314c9f25182b6046126630e9c2f56bd9cb5
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-05-26 18:09:47 +0430

    .

--- scripts/zsh/interactive/completions.zsh
+++ scripts/zsh/interactive/completions.zsh
@@ -1,5 +1,6 @@
-rexa "compdef _=which" cee ceer whichm whdeep whdeep-words wh whh whz lesh mn emn ffman ffcommands rp p f fr tldr re inargs-gen inargsE-gen
-rexa "compdef _=ls" pbcopy mv
+rexa "compdef _=which" cee ceer whichm whdeep whdeep-words wh whh whz lesh emn ffman ffcommands rp p f fr tldr re inargs-gen inargsE-gen
+rexa "compdef _=ls" pbadd mv # mv had a bug I think?
+rexa "compdef _=man" mn
 # rexa "compdef _=xargs"
 isExpensive && {
     [[ -e $asdf_dir ]] && . $asdf_dir/etc/bash_completion.d/asdf.bash
