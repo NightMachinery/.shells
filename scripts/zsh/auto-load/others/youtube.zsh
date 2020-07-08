67cc75afc1a08477cf18529f50aa6c86e7234ed9
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-08 11:56:46 +0430

    .

--- scripts/zsh/auto-load/others/youtube.zsh
+++ scripts/zsh/auto-load/others/youtube.zsh
@@ -2,3 +2,6 @@ function ylist() {
     youtube-dl -j --flat-playlist "$@" | jq -r '"https://youtu.be/\(.id)"'
 }
 noglobfn ylist
+function ytitle() {
+    youtube-dl --get-filename -o "%(title)s" "$@"
+}
