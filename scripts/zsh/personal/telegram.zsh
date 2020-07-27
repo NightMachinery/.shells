680164334bca6aec200b4d94fdca9ff8920fe727
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-27 20:31:45 +0430

    .

--- scripts/zsh/personal/telegram.zsh
+++ scripts/zsh/personal/telegram.zsh
@@ -5,7 +5,7 @@ water='-1001293952668'
 ephemeral='-1001404743282'
 ###
 alice() {
-    tsend -- "$alice" "$*" && ec 'Alicized $* successfully' || ecerr 'Alicization failed!'
+    tsend -- "$alice" "$*" && ec "Alicized $* successfully" || ecerr 'Alicization failed!'
 }
 noglobfn alice
 alias al=alice #NAMECONFLICT: ../Cellar/mono/6.8.0.105/bin/al
