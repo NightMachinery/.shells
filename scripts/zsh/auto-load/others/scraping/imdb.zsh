20df50435aad051fddf6469df66ee2e49d5020bc
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-27 05:48:30 +0430

    .

--- scripts/zsh/auto-load/others/scraping/imdb.zsh
+++ scripts/zsh/auto-load/others/scraping/imdb.zsh
@@ -1,4 +1,8 @@
-imdb1() imdbpy search movie --first "$*"
+function imdb1() {
+    : "DEPRECATED: Use the imdb function"
+
+    imdbpy search movie --first "$*"
+}
 function imdb() {
     # @publishme : needs jq, dash, awk, googler, imdbpy, pcre-enabled zsh
     local query="$* site:imdb.com"
@@ -7,8 +11,12 @@ function imdb() {
     setopt re_match_pcre
     setopt pipefail
 
+    local isI="$(cmd-sub isI true)"
+    local count=4
+    $isI && count=20
+
     local urls url
-    urls=("${(@f)$(ffgoo "$query")}") || return 1
+    urls=("${(@f)$(ffgoo_count=$count ffgoo "$query")}") || return 1
     for url in $urls[@] ; do
         [[ "$url" =~ 'https://www.imdb.com/title/tt(\d+)/?' ]] && {
             imdbpy get movie "$match[1]"
@@ -30,8 +38,10 @@ function cmd-sub() {
         print -nr -- "$sub"
     fi
 }
+##
 function ffgoo() {
     local query="$*"
+    local count="${ffgoo_count:-${ffgoo_c:-30}}"
 
     setopt local_options
     setopt pipefail
@@ -39,7 +49,7 @@ function ffgoo() {
     local fzf_cmd="$(cmd-sub fz fzf)"
     local memoi_cmd="$(cmd-sub memoi-eval '')"
 
-    local search="$($memoi_cmd googler --json --count 30 "$query")"
+    local search="$($memoi_cmd googler --json --count "$count" "$query")"
     local is i
     is=("${(@f)$(<<<$search jq -re '.[] | .title + " | " + .abstract + " | " + .metadata' |cat -n | SHELL=dash $fzf_cmd --multi --preview 'printf -- "%s " {}' --preview-window up:7:wrap --with-nth 2.. | awk '{print $1}')}") || return 1
     for i in $is[@] ; do
