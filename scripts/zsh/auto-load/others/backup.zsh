cb359424fedcee0f921960979996376422916f69
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-31 03:35:07 +0430

    .

--- scripts/zsh/auto-load/others/backup.zsh
+++ scripts/zsh/auto-load/others/backup.zsh
@@ -1,6 +1,6 @@
 function backup-cron() {
     # aka cellar-getcron
-    local d="$cellar/notes/dev/snippets/crontabs/$(hostname)"
+    local d="$nightNotes/backups/crontabs/$(hostname)"
     mkdir -p "$d"
     crontab -l > "${d}/$(whoami)"
 }
