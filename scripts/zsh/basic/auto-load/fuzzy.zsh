2a06e9178ed6b9cd646c5eb4cbbbeb4b27b66030
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-09-06 12:33:57 +0430

    .

--- scripts/zsh/basic/auto-load/fuzzy.zsh
+++ scripts/zsh/basic/auto-load/fuzzy.zsh
@@ -6,19 +6,28 @@ FZF_SIMPLE_PREVIEW='printf -- "%s " {}'
 ##
 ### Functions
 function fz-empty() {
-    fnswap fzf-noempty fzf fz "$@"
+    fz_noempty='' fz "$@"
 }
 function fz() {
-    # "Use `fnswap fzf-noempty gq fz` to get the final command for use in other envs
-    local opts
-    opts=(${(@)fz_opts}) #Don't quote it or it'll insert empty args
+    # "Use `fnswap fzf-gateway gq fz` to get the final command for use in other envs
+    local opts noempty="${fz_noempty:-y}"
+    opts=(${(@)fz_opts} --exit-0) #Don't quote it or it'll insert empty args
+    # --exit-0 : By using this we'll lose the automatic streaming feature of fzf as we need to wait for the whole input. (Update: It doesn't seem that the streaming feature is useful at all, as it doesn't show anything until completion in my tests ...)
+    # --select-1 : auto-selects if only one match
     test -n "$fz_no_preview" || opts+=(--preview "$FZF_SIMPLE_PREVIEW" --preview-window down:7:wrap:hidden)
-    
-    # FNSWAP: fzf-noempty
-    SHELL="${FZF_SHELL:-$(rp dash)}" fzf-noempty "${(@)opts}" "$@" # moved options to FZF_DEFAULT_OPTS
-    # --select-1 
+
+    local cmdbody=( "${(@)opts}" "$@" )
+    if test -n "$noempty" ; then
+        fzf-noempty "$cmdbody[@]" # moved options to FZF_DEFAULT_OPTS
+    else
+        fzf-gateway "$cmdbody[@]"
+    fi
 }
 function fzf-noempty() {
-    local in="$(</dev/stdin)"
-    test -z "$in" && (exit 130) || { ec "$in" | SHELL="${FZF_SHELL:-$(rp dash)}" fzf --exit-0 "$@" }
+    local in="$(</dev/stdin)" # So we need to wait for the whole input to finish first.
+    test -z "$in" && (exit 130) || { ec "$in" | fzf-gateway "$@" }
+}
+function fzf-gateway() {
+    SHELL="${FZF_SHELL:-$(rp dash)}" fzf-tmux -p90% "$@" | sponge
+    # sponge is necessary: https://github.com/junegunn/fzf/pull/1946#issuecomment-687714849
 }
