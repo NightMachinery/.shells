f3fbde0445d204052b7cae7eefe0ded6fd52216b
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-09-09 14:43:35 +0430

    .

--- scripts/zsh/auto-load/others/monitor.zsh
+++ scripts/zsh/auto-load/others/monitor.zsh
@@ -1,3 +1,8 @@
+function glan() {
+    # --time sets the refresh delay in seconds
+    # --byte display network rate in byte per second
+    glances --time 10 --theme-white --disable-webui --fs-free-space --byte --process-short-name "$@"
+}
 function ppgrep() {
     case "$(uname)" in
         Darwin)
---
+++ scripts/zsh/interactive/fuzzy/shortcuts.zsh
@@ -0,0 +1,5 @@
+function openv() {
+    arrN $~videoglob | fz | inargsf mpv
+}
+aliasfn r2 incache openv
+aliasfn lec indir ~/base/Lectures openv
