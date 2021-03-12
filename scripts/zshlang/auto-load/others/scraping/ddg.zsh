dacceff4b06b0bb38ae97a44b9d7c95d8f692c3e
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-03-13 00:04:06 +0330

    modified:   scripts/zshlang/auto-load/others/scraping/ddg.zsh

--- scripts/zshlang/auto-load/others/scraping/ddg.zsh
+++ scripts/zshlang/auto-load/others/scraping/ddg.zsh
@@ -2,13 +2,13 @@ function ddg-html() {
     local query="${1}"
     test -n "$query" || return 1
     
-    eval-memoi withchrome full-html2 'https://duckduckgo.com/?q='"$(ec "$query" | gtr $'\n' ' ' | url-encode.py )"'kp=-2&kl=us-en'
+    revaldbg eval-memoi withchrome full-html2 'https://duckduckgo.com/?q='"$(ec "$query" | gtr $'\n' ' ' | url-encode.py )"'&kp=-2&kl=us-en'
 }
 function ddg-json() {
-    local url="${1}"
-    test -n "$url" || return 1
+    local q="${1}"
+    test -n "$q" || return 1
 
-    ddg-html "$url" | sponge | ddg2json | {
+    ddg-html "$q" | sponge | ddg2json | {
         if isI && isOutTty ; then
             jq .
         else
