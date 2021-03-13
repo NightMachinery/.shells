ed8a0bfe5343ac13979fec3ce3b4d8edb8b93866
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-03-13 16:33:18 +0330

    new file:   scripts/launchers/launch-musicf.zsh; modified:   scripts/launchers/various-zii.zsh

---
+++ scripts/launchers/launch-musicf.zsh
@@ -0,0 +1,88 @@
+#!/usr/bin/env zsh
+
+typeset -A artists
+#### Persian
+artists['Pallett']='https://open.spotify.com/artist/0In2x96q97ZDsP2KALpdVf'
+artists['Eendo']='https://open.spotify.com/artist/6jCwWmmC5b4fF2y0OOP6g8'
+artists['Mohsen Namjoo']='https://open.spotify.com/artist/4eVyI1yiHoRjVrxt5y7gGL'
+artists['Mohsen Chavoshi']='https://open.spotify.com/artist/2IYl0pVatTl6O2d1CQj6GQ'
+artists['Reza Yazdani']='https://open.spotify.com/artist/0tctijgZruddzQ8FCgvDj2'
+###
+artists['Marjan Farsad']='https://open.spotify.com/artist/5sGy7zfcUWYPh3wHbyz4fn'
+artists['Hani Niroo']='https://open.spotify.com/artist/07xBlQ8c7xhTa7IefYshum'
+# artists['Sogand']='https://open.spotify.com/artist/2mGkJsyXC8byI83UHSO4w1'
+artists['Rana Farhan']='https://open.spotify.com/artist/5Fq4u0e9OPI4k4R3QC6OZO'
+## tosee
+artists['Niaz Nawab']='https://open.spotify.com/artist/4hprwICrkEC8ule8JvCcJS'
+artists['Aida Shahghasemi']='https://open.spotify.com/artist/1bQlJQRMtgj4p2gy9nN9Mz'
+artists['Tara Tiba']='https://open.spotify.com/artist/0s6fPszrilIZ2kmduBE9N7'
+artists['Rana Mansour']='https://open.spotify.com/artist/3zrjEdzGRRwrLHv3KQDDuB'
+#### English
+artists['Hollywood Undead']='https://open.spotify.com/artist/0CEFCo8288kQU7mJi25s6E'
+artists['Goran Bregović']='https://open.spotify.com/artist/491v9k5NTGBGanwqPNSkuS'
+artists['The Servant']='https://open.spotify.com/artist/679k99IJomQUuRMbhiohrY'
+artists['Hurts']='https://open.spotify.com/artist/3w4VAlllkAWI6m0AV0Gn6a'
+artists['Alexander Rybak']='https://open.spotify.com/artist/3LLNDXrxL4uxXtnUJS5XWM'
+artists['Pharrell Williams']='https://open.spotify.com/artist/3V38zs2XJF5nXDv5jt31zm'
+artists['Depeche Mode']='https://open.spotify.com/artist/762310PdDnwsDxAQxzQkfX'
+artists['System Of A Down']='https://open.spotify.com/artist/5eAWCfyUhZtHHtBdNk56l1'
+artists['Maroon 5']='https://open.spotify.com/artist/04gDigrS5kc9YWfZHwBETP'
+artists['Owl City']='https://open.spotify.com/artist/07QEuhtrNmmZ0zEcqE9SF6'
+artists['Radiohead']='https://open.spotify.com/artist/4Z8W4fKeB5YxbusRsdQVPb'
+artists['Imagine Dragons']='https://open.spotify.com/artist/53XhwfbYqKCa1cC15pYq2q'
+artists['Marshmello']='https://open.spotify.com/artist/64KEffDW9EtZ1y2vBYgq8T'
+artists['Gary Jules']='https://open.spotify.com/artist/5oRnbmgqvvq7fVlgk4vcEa'
+###
+artists['Shelley Segal']='https://open.spotify.com/artist/3Is6CJbBt3YB6gDxnoNfiE'
+artists['Sia']='https://open.spotify.com/artist/5WUlDfRSoLAfcVSX1WnrxN'
+artists['The Tiger Lillies']='https://open.spotify.com/artist/72o2T9aqYR2PFNYpzYrMmw'
+artists['Portishead']='https://open.spotify.com/artist/6liAMWkVf5LH7YR9yfFy1Y'
+artists['Madonna']='https://open.spotify.com/artist/6tbjWDEIzxoDsBA1FuhfPW'
+# artists['Becky G']='https://open.spotify.com/artist/4obzFoKoKRHIphyHzJ35G3'
+artists['Halsey']='https://open.spotify.com/artist/26VFTg2z8YR0cCuwLzESi2'
+artists['AURORA']='https://open.spotify.com/artist/1WgXqy2Dd70QQOU7Ay074N'
+artists['Selena Gomez']='https://open.spotify.com/artist/0C8ZW7ezQVs4URX5aX7Kqx'
+artists['Demi Lovato']='https://open.spotify.com/artist/6S2OmqARrzebs0tKUEyXyp'
+artists['Billie Eilish']='https://open.spotify.com/artist/6qqNVTkY8uBg9cP3Jd7DAH'
+artists['Lily Allen']='https://open.spotify.com/artist/13saZpZnCDWOI9D4IJhp1f'
+artists['Lana Del Rey']='https://open.spotify.com/artist/00FQb4jTyendYWaN8pK0wa'
+artists['Ariana Grande']='https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR'
+# artists['INNA']='https://open.spotify.com/artist/2w9zwq3AktTeYYMuhMjju8'
+artists['Lorde']='https://open.spotify.com/artist/163tK9Wjr9P9DmM0AVK7lm'
+artists['Melanie Martinez']='https://open.spotify.com/artist/63yrD80RY3RNEM2YDpUpO8'
+# artists['Taylor Swift']='https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02'
+artists['Dido']='https://open.spotify.com/artist/2mpeljBig2IXLXRAFO9AAs'
+artists['Carly Rae Jepsen']='https://open.spotify.com/artist/6sFIWsNpZYqfjUpaCgueju'
+## tosee
+artists['Hey Violet']='https://open.spotify.com/artist/4JNfz6aO9ZFz0gp5GY88am'
+artists['Cavetown']='https://open.spotify.com/artist/2hR4h1Cao2ueuI7Cx9c7V8'
+artists['Camila Cabello']='https://open.spotify.com/artist/4nDoRrQiYLoBzwC5BhVJzF'
+artists['Fifth Harmony']='https://open.spotify.com/artist/1l8Fu6IkuTP0U5QetQJ5Xt'
+#### French
+artists['Cœur De Pirate']='https://open.spotify.com/artist/2eRNMtoi82UZUuaL6naDjA'
+#### Asian
+artists['BLACKPINK']='https://open.spotify.com/artist/41MozSoPIsD1dJM0CLPjZF'
+artists['f(x)']='https://open.spotify.com/artist/3wRA5UYoo08BBKJnzyKkpF'
+#### Japanese
+artists['Ali Project']='https://open.spotify.com/artist/1kCSjswFJFcjTauk0sKaOn'
+artists['Kalafina']='https://open.spotify.com/artist/2otsTXVV2ZWZ8T5LPzsBhy'
+artists['EGOIST']='https://open.spotify.com/artist/0k7JZhYS35IewiKNHW7KMj'
+artists['YUI']='https://open.spotify.com/artist/5WBO8UyOuJ1l7ZBqqBimpO'
+artists['LiSA']='https://open.spotify.com/artist/0blbVefuxOGltDBa00dspv'
+artists['Luna Haruna']='https://open.spotify.com/artist/4eL15GpHSLAAvv5sT00c5i'
+artists['Kanako Itō']='https://open.spotify.com/artist/1ioBqDTNhnBVubB4ceQ7AI'
+artists['Konomi Suzuki']='https://open.spotify.com/artist/3O0kLS5sLZ41XZRxjBKmLF'
+artists['Eir Aoi']='https://open.spotify.com/artist/18moNotPmBWa2YZtRnIoZ3'
+## tosee
+artists['Tia']='https://open.spotify.com/artist/30rNCtpbgMaznRM9h8jlLv'
+#### Instrumental-ish
+# artists['Brian Eno']='https://open.spotify.com/artist/7MSUfLeTdDEoZiJPDSBXgi'
+artists['KOKIA']='https://open.spotify.com/artist/0gUsuVOGa73jzUAJ0WEJgo'
+artists['Ryuichi Sakamoto']='https://open.spotify.com/artist/1tcgfoMTT1szjUeaikxRjA'
+artists['Dream Theater']='https://open.spotify.com/artist/2aaLAng2L2aWD2FClzwiep'
+artists['Sleep Party People']='https://open.spotify.com/artist/6BpaTDEjadTL9Plx4VUKvo'
+artists['iday']='https://open.spotify.com/artist/6eBKd6f53rieNg2THLwnFQ'
+artists['Infected Mushroom']='https://open.spotify.com/artist/6S2tas4z6DyIklBajDqJxI'
+####
+# artists['']=''
+tmuxnewshenv="rt_skip='$rt_skip' rt_e=(rss-engine-spotify) rt_notel=y rt_nt=y rt_ge=(spotify-discography-get) rt_eud=0 rt_eid=$((3600*24*14))" tmuxnewsh musicf-spotify rss-tsend ${(@v)artists}
--- scripts/launchers/various-zii.zsh
+++ scripts/launchers/various-zii.zsh
@@ -17,58 +17,6 @@ tmuxnew v2ray v2ray -config /usr/local/etc/v2ray/config.json
 # Currently managed by systemd:
 # tmuxnew trojan trojan --config /usr/local/etc/trojan/config.json
 ##
-typeset -A artists
-#### Persian
-artists['Mohsen Namjoo']='https://open.spotify.com/artist/4eVyI1yiHoRjVrxt5y7gGL'
-artists['Mohsen Chavoshi']='https://open.spotify.com/artist/2IYl0pVatTl6O2d1CQj6GQ'
-artists['Reza Yazdani']='https://open.spotify.com/artist/0tctijgZruddzQ8FCgvDj2'
-###
-artists['Marjan Farsad']='https://open.spotify.com/artist/5sGy7zfcUWYPh3wHbyz4fn'
-artists['Hani Niroo']='https://open.spotify.com/artist/07xBlQ8c7xhTa7IefYshum'
-# artists['Sogand']='https://open.spotify.com/artist/2mGkJsyXC8byI83UHSO4w1'
-artists['Rana Farhan']='https://open.spotify.com/artist/5Fq4u0e9OPI4k4R3QC6OZO'
-##
-artists['Niaz Nawab']='https://open.spotify.com/artist/4hprwICrkEC8ule8JvCcJS'
-artists['Aida Shahghasemi']='https://open.spotify.com/artist/1bQlJQRMtgj4p2gy9nN9Mz'
-artists['Tara Tiba']='https://open.spotify.com/artist/0s6fPszrilIZ2kmduBE9N7'
-artists['Rana Mansour']='https://open.spotify.com/artist/3zrjEdzGRRwrLHv3KQDDuB'
-#### English
-artists['Alexander Rybak']='https://open.spotify.com/artist/3LLNDXrxL4uxXtnUJS5XWM'
-artists['Pharrell Williams']='https://open.spotify.com/artist/3V38zs2XJF5nXDv5jt31zm'
-artists['Depeche Mode']='https://open.spotify.com/artist/762310PdDnwsDxAQxzQkfX'
-artists['System Of A Down']='https://open.spotify.com/artist/5eAWCfyUhZtHHtBdNk56l1'
-artists['Maroon 5']='https://open.spotify.com/artist/04gDigrS5kc9YWfZHwBETP'
-artists['Owl City']='https://open.spotify.com/artist/07QEuhtrNmmZ0zEcqE9SF6'
-##
-# artists['Brian Eno']='https://open.spotify.com/artist/7MSUfLeTdDEoZiJPDSBXgi'
-###
-# artists['Becky G']='https://open.spotify.com/artist/4obzFoKoKRHIphyHzJ35G3'
-artists['Halsey']='https://open.spotify.com/artist/26VFTg2z8YR0cCuwLzESi2'
-artists['AURORA']='https://open.spotify.com/artist/1WgXqy2Dd70QQOU7Ay074N'
-artists['Selena Gomez']='https://open.spotify.com/artist/0C8ZW7ezQVs4URX5aX7Kqx'
-artists['Demi Lovato']='https://open.spotify.com/artist/6S2OmqARrzebs0tKUEyXyp'
-artists['Billie Eilish']='https://open.spotify.com/artist/6qqNVTkY8uBg9cP3Jd7DAH'
-artists['Lily Allen']='https://open.spotify.com/artist/13saZpZnCDWOI9D4IJhp1f'
-artists['Lana Del Rey']='https://open.spotify.com/artist/00FQb4jTyendYWaN8pK0wa'
-artists['Ariana Grande']='https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR'
-# artists['INNA']='https://open.spotify.com/artist/2w9zwq3AktTeYYMuhMjju8'
-artists['Lorde']='https://open.spotify.com/artist/163tK9Wjr9P9DmM0AVK7lm'
-artists['Melanie Martinez']='https://open.spotify.com/artist/63yrD80RY3RNEM2YDpUpO8'
-# artists['Taylor Swift']='https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02'
-artists['Dido']='https://open.spotify.com/artist/2mpeljBig2IXLXRAFO9AAs'
-artists['Carly Rae Jepsen']='https://open.spotify.com/artist/6sFIWsNpZYqfjUpaCgueju'
-##
-artists['Hey Violet']='https://open.spotify.com/artist/4JNfz6aO9ZFz0gp5GY88am'
-artists['Cavetown']='https://open.spotify.com/artist/2hR4h1Cao2ueuI7Cx9c7V8'
-artists['Camila Cabello']='https://open.spotify.com/artist/4nDoRrQiYLoBzwC5BhVJzF'
-artists['Fifth Harmony']='https://open.spotify.com/artist/1l8Fu6IkuTP0U5QetQJ5Xt'
-#### French
-artists['Cœur De Pirate']='https://open.spotify.com/artist/2eRNMtoi82UZUuaL6naDjA'
-#### Asian
-artists['BLACKPINK']='https://open.spotify.com/artist/41MozSoPIsD1dJM0CLPjZF'
-artists['f(x)']='https://open.spotify.com/artist/3wRA5UYoo08BBKJnzyKkpF'
-####
-# artists['']=''
-tmuxnewshenv="rt_skip='$rt_skip' rt_e=(rss-engine-spotify) rt_notel=y rt_nt=y rt_ge=(spotify-discography-get) rt_eud=0 rt_eid=$((3600*24*14))" tmuxnewsh rss-spotify rss-tsend ${(@v)artists}
+launch-musicf.zsh
 ##
 chronic-all
