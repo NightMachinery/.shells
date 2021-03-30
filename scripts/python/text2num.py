3e6bd207725fae89476ee5745aff8c64bbd31027
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-03-30 19:36:58 +0430

    adding borg-tt-cmd-log; fixed bug in text2num

--- scripts/python/text2num.py
+++ scripts/python/text2num.py
@@ -4,7 +4,9 @@ from text_to_num import alpha2digit
 import sys
 
 text = sys.stdin.read()
-if text.strip() == 'o': # replaces this with 0, stupid algo
-    print(text)
-    sys.exit(0)
-print(alpha2digit(text, "en"))
+text = text.split('\n')
+for line in text:
+    if line.strip() == 'o': # replaces this with 0, stupid algo
+        print(line)
+        continue
+    print(alpha2digit(line, "en"))
--- scripts/zshlang/auto-load/others/borg/api/timetracker.zsh
+++ scripts/zshlang/auto-load/others/borg/api/timetracker.zsh
@@ -4,6 +4,10 @@ function borg-req() {
 ##
 function borg-tt-mark() {
     local received_at="${borg_tt_mark_at}"
+    if isLocal ; then
+        revaldbg brishzr @opts at "$received_at" @ "$0" "$@"
+        return $?
+    fi
 
 	local out
 	out="$(ec "$*" | text2num | jq --raw-input --arg received_at "$received_at" --slurp --null-input --compact-output 'inputs as $i | {"name": $i, "received_at": $received_at}' | borg-req timetracker/mark/)" || return $?
@@ -19,5 +23,25 @@ function borg-tt-last() {
 }
 ##
 function borg-tt-cmdlog() {
+    local input="$(cat)"
 
+    local received_at cmd=() i
+    for i in ${(@f)input} ; do # skips empty lines
+        if [[ "$i" =~ '^\s*:::(.*)' ]] ; then
+            if test -n "$received_at" && (( $#cmd >= 1 )) ; then
+                reval-ec @opts at "$received_at" @ borg-tt-mark "${(@F)cmd}"
+            fi
+            received_at="$match[1]"
+            cmd=()
+        elif test -z "$received_at" ; then
+            ecerr "input does not begin with received_at"
+            return 1
+        else
+            cmd+="$i"
+        fi
+    done
+    # @copypaste :
+    if test -n "$received_at" && (( $#cmd >= 1 )) ; then
+        reval-ec @opts at "$received_at" @ borg-tt-mark "${(@F)cmd}"
+    fi
 }
