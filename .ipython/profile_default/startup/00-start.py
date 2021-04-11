4268436988d40d9576c9ecfa3960c6817bdfc997
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-04-12 00:49:35 +0430

    modified: 00-start.py

--- .ipython/profile_default/startup/00-start.py
+++ .ipython/profile_default/startup/00-start.py
@@ -23,6 +23,19 @@ except ImportError:  # Graceful fallback if IceCream isn't installed.
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
 ###
+from functools import wraps
+from time import time
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func:%r args:[%r, %r] took: %2.4f sec' % (f.__name__, args, kw, te-ts))
+        return result
+    return wrap
+###
 def char_range(c1, c2):
     """Generates the characters from `c1` to `c2`, inclusive."""
     for c in range(ord(c1), ord(c2) + 1):
