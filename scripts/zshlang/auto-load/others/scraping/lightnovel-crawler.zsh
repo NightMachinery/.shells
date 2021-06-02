b7c3bedcfbd1fee1ad0ee02b858e4bf9ad1156b2
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-06-02 21:45:24 +0430

    small changes

--- scripts/zshlang/auto-load/others/scraping/lightnovel-crawler.zsh
+++ scripts/zshlang/auto-load/others/scraping/lightnovel-crawler.zsh
@@ -3,5 +3,13 @@ function lnc-epub {
     local url="$1"
     assert-args url @RET
 
-    lightnovel-crawler --all --single --format epub --suppress --ignore -o . --source "$url"
+    lightnovel-crawler --all --single --format epub --suppress --source "$url"
+    # --ignore -o .
+
+    if isJulia ; then
+        jup
+        p2k *.epub
+    fi
 }
+alias lnc='lnc-epub'
+##
