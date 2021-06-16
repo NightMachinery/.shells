dfe15310b15426004e42e4696bca8d3ce5b95387
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-06-16 18:50:35 +0430

    small changes

--- scripts/setup/setup_sudo.zsh
+++ scripts/setup/setup_sudo.zsh
@@ -1,11 +1,14 @@
 #!/usr/bin/env zsh
 
 ##
-if isLinux ; then
-    sudo apt remove mosh || true
-    # use brew to install the HEAD: `brm mosh ; bi mosh --head`
-fi
-sudo rm -f /usr/local/bin/mosh-server
-rehash
-reval-ec sudo ln -s "${commands[mosh-server]}" /usr/local/bin/
+function mosh-ins {
+    if isLinux ; then
+        sudo apt remove mosh || true
+        # use brew to install the HEAD:
+        brew remove mosh ; brew install mosh --head
+    fi
+    sudo rm -f /usr/local/bin/mosh-server
+    rehash
+    reval-ec sudo ln -s "${commands[mosh-server]}" /usr/local/bin/
+}
 ##
