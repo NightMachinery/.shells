dbdb3f9b27bc3891fe2d05f01d6d12c6c845e2d7
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-06-20 01:28:07 +0430

    cl: created usable brishz-ll, brishz; made fanficfare2org usable and added emacs integration

--- scripts/zshlang/auto-load/others/scraping/fanfic.zsh
+++ scripts/zshlang/auto-load/others/scraping/fanfic.zsh
@@ -2,5 +2,9 @@ function fanficfare2org {
     local url="$1"
     assert-args url @RET
 
-    fanficfare --meta-only --json-meta "$url" | fanficfare2org.lisp
+    local d
+    # fanficfare still creates a junk epub file with --meta-only
+    d="$(indir "$(gmktemp -d)" fanficfare --meta-only --json-meta "$url")" @TRET
+
+    ec "$d" | fanficfare2org.lisp @TRET
 }
