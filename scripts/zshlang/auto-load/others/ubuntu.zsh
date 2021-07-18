321c42fa1c2a3147141205e0986d7ae949d95d9a
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-07-18 19:39:48 +0430

    small changes

--- scripts/zshlang/auto-load/others/ubuntu.zsh
+++ scripts/zshlang/auto-load/others/ubuntu.zsh
@@ -16,7 +16,10 @@ function ubuntu-upgrade() {
 
     sudo DEBIAN_FRONTEND='noninteractive' apt-get --fix-missing -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' autoclean
     
-    # sudo do-release-upgrade # -d forces a develop upgrade
-    # upgrading has a lot of interactive prompts:
-    # https://stackoverflow.com/questions/62192742/noninteractive-do-release-upgrade-with-old-configs-by-default
+    ecerr "$0:"' Use `sudo do-release-upgrade # -d forces a develop upgrade`
+    @warn Upgrading has a lot of interactive prompts:
+    https://stackoverflow.com/questions/62192742/noninteractive-do-release-upgrade-with-old-configs-by-default
+
+    You can use `do-release-upgrade -f DistUpgradeViewNonInteractive`, which replaces old config files by the new default files. This seems to also severely reduce the log verbosity.
+'
 }
