ae5633c82b6b727debd3d2b8befd5f20e281f3d9
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-08-30 17:35:30 +0430

    increasing compat

--- scripts/zshlang/basic/deps.zsh
+++ scripts/zshlang/basic/deps.zsh
@@ -2,8 +2,12 @@ function ensure-dep-pdftk {
     ensure-dep1 pdftk brew install pdftk-java || return $?
 }
 ##
+function kitty-theme-install {
+    pip-install psutil 'git+git://github.com/fretboardfreak/kitty-theme-changer.git@master'
+}
+
 function ensure-dep-kitty-theme {
-    ensure-dep1 kitty-theme pip-install git+git://github.com/fretboardfreak/kitty-theme-changer.git@master || return $?
+    ensure-dep1 kitty-theme kitty-theme-install || return $?
 }
 ##
 function bicon-install {
