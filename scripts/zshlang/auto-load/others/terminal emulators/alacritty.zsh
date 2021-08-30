c50df415e710c656cc6f0759112b5c3a5485e25a
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-08-30 21:17:37 +0430

    increasing compat

--- scripts/zshlang/auto-load/others/terminal emulators/alacritty.zsh
+++ scripts/zshlang/auto-load/others/terminal emulators/alacritty.zsh
@@ -8,14 +8,15 @@ function alacritty-theme-setup {
     assert git clone $REPO $DEST @RET
     # Create symlink at default colors location (optional)
     assert ln -s "$DEST/themes" "$HOME/.config/alacritty/colors" @RET
+    assert touch "$HOME/.config/alacritty/alacritty.yml" @RET # expects this file to exist
     ##
 }
 ##
-# ALACRITTY_LIGHT_COLOR='gotham.yml'
-# ALACRITTY_DARK_COLOR='papercolor_light.yml'
+# ALACRITTY_LIGHT_COLOR='gotham.yaml'
+# ALACRITTY_DARK_COLOR='papercolor_light.yaml'
 ##
-ALACRITTY_LIGHT_COLOR='solarized_light.yml'
-ALACRITTY_DARK_COLOR='solarized_dark.yml'
+ALACRITTY_LIGHT_COLOR='solarized_light.yaml'
+ALACRITTY_DARK_COLOR='solarized_dark.yaml'
 ##
 function alacritty-colorscheme {
     ensure-dep1 alacritty-colorscheme alacritty-theme-setup @RET
