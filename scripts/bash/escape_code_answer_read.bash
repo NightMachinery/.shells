0a3a665d47de3c0847c4273e72aa720d66662a54
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-08-31 18:13:50 +0430

    added escape-code-answer-read, term-get

--- scripts/bash/escape_code_answer_read.bash
+++ scripts/bash/escape_code_answer_read.bash
@@ -3,11 +3,13 @@
 # adapted from:
 # https://stackoverflow.com/questions/47938109/reading-answer-to-control-string-sent-to-xterm
 ##
+# wrapped by 'escape-code-answer-read' in zsh.
+##
 # usage examples:
 # `printf '\eP+q544e\e\\' | escape_code_answer_read.bash`
 ##
 tty=/dev/tty
 
 cat >$tty
-read -rs -t 0.2 -d "" <$tty
+read -rs -t "${escape_code_answer_read_timeout:-0.2}" -d "" <$tty # the timeout needs to be big, so that it would work via SSH, too
 echo $REPLY | xxd -r -p
