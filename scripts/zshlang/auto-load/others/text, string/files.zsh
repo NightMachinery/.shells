4ed67a75770b028fdcf337aa9fec7052b332a4ca
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-09-20 20:00:12 +0430

    fixed regex-mode pre-files

--- scripts/zshlang/auto-load/others/text, string/files.zsh
+++ scripts/zshlang/auto-load/others/text, string/files.zsh
@@ -1,20 +1,29 @@
 ### Module Text Processing
 ### This module specializes in functions that touch the disk.
 ###
-pre-files() {
-    doc 'stdin should be null-separated list of files that need replacement; $1 the string to replace, $2 the replacement.'
-    comment '-i backs up original input files with the supplied extension (leave empty for no backup; needed for in-place replacement.)(do not put whitespace between -i and its arg.)'
-    comment '-r, --no-run-if-empty
-              If  the  standard input does not contain any nonblanks,
-              do not run the command.  Normally, the command  is  run
-              once  even  if there is no input.  This option is a GNU
-              extension.'
+function pre-files() {
+  doc 'stdin should be null-separated list of files that need replacement; $1 the string to replace, $2 the replacement.'
 
-    local cmd='s/\Q$ENV{AGR_FROM}\E/$ENV{AGR_TO}/g'
-    if test -n "$agr_regex" ; then
+  local from="$1" to="$2"
+
+  local cmd
+  if test -n "$agr_regex" ; then
+    if true ; then
+    cmd="s/${from}/${to}/g"
+    else
+      # the quoting rules here are weird, and seem to quote AGR_TO as a literal string.
       cmd='s/$ENV{AGR_FROM}/$ENV{AGR_TO}/g'
     fi
-    AGR_FROM="$1" AGR_TO="$2" gxargs -r0 perl -pi"$pf_i" -e "$cmd"
+  else
+    cmd='s/\Q$ENV{AGR_FROM}\E/$ENV{AGR_TO}/g'
+  fi
+
+  AGR_FROM="$from" AGR_TO="$to" gxargs -r0 perl -pi"$pf_i" -e "$cmd" @RET
+  # -i:
+  # backs up original input files with the supplied extension (leave empty for no backup; needed for in-place replacement.)(do not put whitespace between -i and its arg.)'
+  #
+  # -r, --no-run-if-empty:
+  # If  the  standard input does not contain any nonblanks, do not run the command.  Normally, the command  is  run once  even  if there is no input.  This option is a GNU extension.'
 }
 @opts-setprefix pre-files agr
 ##
