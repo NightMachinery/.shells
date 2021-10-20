32095fe7820dd9de5ea5a36d643631dc3c140ac6
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-10-20 19:23:35 +0330

    org_sort_date now works, and nt-due-oh uses it and a link title cleaner to postprocess.

--- scripts/cpplang/org_sort_date/main.cpp
+++ scripts/cpplang/org_sort_date/main.cpp
@@ -1,4 +1,3 @@
-#include <cstring>
 #include <iostream>
 #include <cstdio>
 #include <string>
@@ -9,7 +8,7 @@
 
 using namespace std;
 
-boost::locale::date_time from_jalali_to_gregorian(const int& year, const int& month, const int& day)
+boost::locale::date_time jalali_to_gregorian(const int& year, const int& month, const int& day)
 {
     boost::locale::generator generator;
 
@@ -29,12 +28,12 @@ boost::locale::date_time from_jalali_to_gregorian(const int& year, const int& mo
 
 void date_print(const boost::locale::date_time& date) {
     std::cout
-              << (date.get(boost::locale::period::year()))
-              << "/" << std::setfill('0') << std::setw(2)
-              << (date.get(boost::locale::period::month()) + 1)
-              << "/" << std::setfill('0') << std::setw(2)
-              << (date.get(boost::locale::period::day()))
-              << "." << std::endl;
+        << (date.get(boost::locale::period::year()))
+        << "/" << std::setfill('0') << std::setw(2)
+        << (date.get(boost::locale::period::month()) + 1)
+        << "/" << std::setfill('0') << std::setw(2)
+        << (date.get(boost::locale::period::day()))
+        << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -42,21 +41,24 @@ int main(int argc, char** argv) {
     string s{begin, end};
 
     boost::regex root_heading_re{"^\\*\\s+"};
-    boost::sregex_token_iterator root_heading_block{s.begin(), s.end(), root_heading_re, -1}, regex_end{};
+    boost::sregex_token_iterator root_heading_block_iter{s.begin(), s.end(), root_heading_re, -1}, regex_end{};
     // When class regex_token_iterator is used to enumerate a single sub-expression with index -1, then the iterator performs field splitting: that is to say it enumerates one character sequence for each section of the character container sequence that does not match the regular expression specified.
 
     boost::regex date_re{"(?:\\[|<)((?:jalali):([^]/]+)(?:/([^]/]+))?(?:/([^]/]+))?(?:/([^]]+))?)(?:\\]|>)",
     boost::regex::normal | boost::regbase::icase};
 
-    while (root_heading_block!=regex_end) {
-        const string& found = *root_heading_block++;
+    vector<tuple<string, boost::locale::date_time>> blocks{};
+    while (root_heading_block_iter!=regex_end) {
+        const string& block_current = *root_heading_block_iter++;
 
-        // cout << "Block:\n" << found << endl; // @ic
+        // cout << "Block:\n" << block_current << endl; // @ic
 
         const int subs[]{1,2,3,4};
-        boost::sregex_token_iterator date_iter{found.begin(), found.end(), date_re, subs};
+        boost::sregex_token_iterator date_iter{block_current.begin(), block_current.end(), date_re, subs};
+
+        vector<boost::locale::date_time> dates{};
         while (date_iter!=regex_end) {
-            const string& date = *date_iter++;
+            const string& link_str = *date_iter++;
             const string& year = *date_iter++;
             const string& month = *date_iter++;
             const string& day = *date_iter++;
@@ -65,19 +67,40 @@ int main(int argc, char** argv) {
             const int month_int = atoi(month.c_str());
             const int day_int = atoi(day.c_str());
 
-            cout << date << endl;
-            cout << "year: " << year << endl;
-            cout << "year_int: " << year_int << endl;
-            cout << "month: " << month << endl;
-            cout << "month_int: " << month_int << endl;
-            cout << "day: " << day << endl;
-            cout << "day_int: " << day_int << endl;
+            if (false) { // @ic
+                cout << link_str << endl;
 
-            const boost::locale::date_time& date_norm = from_jalali_to_gregorian(year_int, month_int, day_int);
+                cout << "year: " << year << endl;
+                cout << "year_int: " << year_int << endl;
+                cout << "month: " << month << endl;
+                cout << "month_int: " << month_int << endl;
+                cout << "day: " << day << endl;
+                cout << "day_int: " << day_int << endl;
+            }
 
-            date_print(date_norm);
+            const boost::locale::date_time& date_norm = jalali_to_gregorian(year_int, month_int, day_int);
+
+            dates.push_back(date_norm);
+        }
+
+        vector<boost::locale::date_time>::iterator last_date_iter = max_element(dates.begin(), dates.end());
+        if (last_date_iter==dates.end()) {
+            continue;
+        } else {
+            const auto& last_date = *last_date_iter;
+            // date_print(last_date); // @ic
+
+            blocks.emplace_back(make_tuple(block_current, last_date));
         }
     }
 
+    sort(blocks.begin(), blocks.end(), [](tuple<string, boost::locale::date_time> a, tuple<string, boost::locale::date_time> b) {
+        return get<1>(a) < get<1>(b);
+    });
+
+    for (const tuple<string, boost::locale::date_time>& block : blocks) {
+        const string& block_str = get<0>(block);
+        cout << "* " << block_str;
+    }
     return 0;
 }
