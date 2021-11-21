66908fe02bd4f1d7a0f7a261d5633a1f2becffda
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-11-22 01:38:22 +0330

    Forked: mneri/pnglatex. Added: tex2png, mktemp-borg.

---
+++ scripts/zshlang/auto-load/others/latex.zsh
@@ -0,0 +1,17 @@
+##
+function tex2png {
+    local tex
+    tex="$(in-or-args "$@")" @RET
+    local dpi="${tex2png_dpi:-1500}"
+    local dest
+    dest="${tex2png_o:-$(mktemp-borg --suffix='.png')}" @TRET
+    local pkgs=( ${tex2png_pkgs[@]} amsmath )
+
+    pnglatex.bash -b Transparent -P 2 -p "${(j.:.)pkgs}" -d "$dpi" -o "$dest" -f "$tex" @TRET
+
+    if isIReally ; then
+        icat "$dest" || true
+    fi
+}
+alias xt='\noglob tex2png' #: =\= still needs escaping
+##
