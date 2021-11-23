dc153e147a9654f8a56130a4216e22a6209f1b4d
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-11-23 19:36:11 +0330

    Added: arxiv-vanity. Fixed: jinit.

--- scripts/zshlang/auto-load/others/scraping/arxiv.zsh
+++ scripts/zshlang/auto-load/others/scraping/arxiv.zsh
@@ -3,18 +3,27 @@ function arxiv-dl {
     local url="${1}" #: Example: =https://arxiv.org/abs/2109.02355=
     assert-args url @RET
     local dest="${arxiv_dl_o}"
+    local mode="${arxiv_dl_mode:-pdf}"
 
-    local url_pdf title
-    if [[ "$url" =~ '/(?:abs|pdf)/([^/]+)/*$' ]] ; then
-        url_pdf="https://arxiv.org/pdf/${match[1]}"
+    local id url_pdf url_abs url_vanity title
+    if [[ "$url" =~ '/(?:abs|pdf)/([^/]+?)(?:\.pdf)?/*$' ]] ; then
+        id="${match[1]}"
+        url_pdf="https://arxiv.org/pdf/${id}"
+        url_abs="https://arxiv.org/abs/${id}"
+        url_vanity="https://arxiv-vanity.com/papers/${id}"
 
-        if test -z "$dest" ; then
-            title="$(url-title "$url" | str2filename)" @TRET
-            dest="${title}.pdf"
+
+        if [[ "$mode" == 'pdf' ]] ; then
+            if test -z "$dest" ; then
+                title="$(url-title "$url" | str2filename)" @TRET
+                dest="${title}.pdf"
+            fi
+
+            revaldbg aa-2dest "$url_pdf" "$dest" >&2 @TRET
+            ec "$dest"
+        elif [[ "$mode" == 'vanity' ]] ; then
+            web2pdf "$url_vanity"
         fi
-
-        revaldbg aa-2dest "$url_pdf" "$dest" >&2 @TRET
-        ec "$dest"
     else
         ecerr "$0: Bad URL $(gq "$url")"
         return 1
@@ -22,12 +31,15 @@ function arxiv-dl {
 }
 renog arxiv-dl
 
-function arxiv2k {
+aliasfn-ng arxiv-vanity arxiv_dl_mode=vanity arxiv-dl
+aliasfn-ng arxv arxiv-vanity
+
+function arxiv-k2 {
     local url="${1}"
     assert-args url @RET
 
     local dled
-    dled="$(arxiv-dl "$url")" @TRET
+    dled="$(arxiv_dl_mode=pdf arxiv-dl "$url")" @TRET
     k2pdf "$dled" @TRET
     local converted
     converted="${dled:r}_k2opt.pdf"
