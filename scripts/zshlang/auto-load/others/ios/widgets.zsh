86482f5e3515b20207fd497eb70ae81dc3b9627a
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-02-23 19:27:51 +0330

    small changes

--- scripts/zshlang/auto-load/others/ios/widgets.zsh
+++ scripts/zshlang/auto-load/others/ios/widgets.zsh
@@ -1,6 +1,8 @@
+##
 function iwidget-rem() {
     datej-all
     ec
     # Beware that the cache hides the newly added upcoming events. Use `iwidget-rem-refresh`.
     @opts key "$(date '+%Y/%m/%d')" @ memoi-eval rem-summary
 }
+##
