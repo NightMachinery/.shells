d5c3c2454f28228cb84f8c05e841daf78b161b39
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-04-06 15:26:58 +0430

    switched rsp-dl et al to aliasfn

--- scripts/zshlang/auto-load/others/rsync.zsh
+++ scripts/zshlang/auto-load/others/rsync.zsh
@@ -1,6 +1,6 @@
-alias rsp-dl='rsync --protect-args --human-readable --xattrs --times --info=progress2 --append -r' # append assumes files only grow and do not otherwise change
-alias rsp-safe='rsync --checksum --protect-args --human-readable --xattrs --times --info=progress2 --partial-dir=.rsync-partial -r' # partial-dir supports resume
-alias rsp='rsp-safe --delete-after --force-delete' #--ignore-errors will delete even if there are IO errors on sender's side.
-alias rspm='rsp --crtimes'
-alias rspb='rsp --backup --backup-dir=.rsync-backup'
-alias rspbm='rspb --crtimes'
+aliasfn rsp-dl rsync --protect-args --human-readable --xattrs --times --info=progress2 --append -r # append assumes files only grow and do not otherwise change
+aliasfn rsp-safe rsync --checksum --protect-args --human-readable --xattrs --times --info=progress2 --partial-dir=.rsync-partial -r # partial-dir supports resume
+aliasfn rsp rsp-safe --delete-after --force-delete #--ignore-errors will delete even if there are IO errors on sender's side.
+aliasfn rspm rsp --crtimes
+aliasfn rspb rsp --backup --backup-dir=.rsync-backup
+aliasfn rspbm rspb --crtimes
