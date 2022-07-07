76f214d8893cf356cbdc8cf9943092d6766e9006
NightMachinery <rudiwillalwaysloveyou@gmail.com>
2022-07-07 10:49:31 +0430

    renamed powerdecay to power-monitor-darwin

--- scripts/zshlang/auto-load/others/monitor/battery.zsh
+++ scripts/zshlang/auto-load/others/monitor/battery.zsh
@@ -10,3 +10,8 @@ function battery-status-darwin-full() {
 function battery-status-darwin() {
     battery-status-darwin-full | rget '(\d+%);'
 }
+##
+function power-monitor-darwin {
+    sudo powermetrics -i 1000 --poweravg 1 | grep 'Average cumulatively decayed power score' -A 20
+}
+##
