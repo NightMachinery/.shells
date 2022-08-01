9881414326983ee15354b237b08de310271e9a57
NightMachinery <rudiwillalwaysloveyou@gmail.com>
2022-08-01 23:28:55 +0430

    small changes

---
+++ scripts/zshlang/disabled/tmp.zsh
@@ -0,0 +1,10 @@
+function fz-rtl1() {
+    local input="$(cat)" q="$1" opts=("${@[2,-1]}")
+
+    local sels_i="$(echo "$input" |
+                     fribidi --nobreak |
+                     cat -n |
+                     fzf --with-nth 2.. --query "$q" "$opts[@]" |
+                     awk '{print $1}')"
+    awk 'NR == FNR {nums[$1]; next} FNR in nums' <(ec "$sels_i") <(ec "$input")
+}
