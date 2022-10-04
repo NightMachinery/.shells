85ff932b8fca0f27b77295480e319ae57a91b930
NightMachinery <rudiwillalwaysloveyou@gmail.com>
2022-10-04 15:13:58 +0330

    added golang-install

--- scripts/zshlang/basic/deps.zsh
+++ scripts/zshlang/basic/deps.zsh
@@ -24,6 +24,27 @@ function bicon-install {
     reval-ec sudo make install ||true # will error, but it will install bicon.bin
 }
 ##
+function golang-install {
+    #: https://go.dev/doc/install
+    #: https://files.lilf.ir/go1.19.1.linux-amd64.tar.gz
+    ##
+    local v="${1:-go1.19.1.linux-amd64.tar.gz}"
+    if [[ "$v" != http* ]] ; then
+        v="https://golang.org/dl/${v}"
+    fi
+    local name="${v:t}"
+    name="${name:-go.tar.gz}"
+
+    local d=~/code
+
+    mkdir -p "$d" || return $?
+    cd "$d" || return $?
+    rm -rf "./${name}" && curl -L --output "$name" "${v}" || return $?
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "./${name}" || return $?
+
+    echo "You might need to add 'export PATH=\"\$PATH:/usr/local/go/bin\"'." >&2
+}
+##
 function caddy-install {
     assert go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest @RET
 
