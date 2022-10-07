9376646457ddf15934aeb425b538cfa22a4b0b62
NightMachinery <rudiwillalwaysloveyou@gmail.com>
2022-10-07 17:45:19 +0330

    added org-export-recursive, org-link-extract-file, org-link-extract-id, rsp-notes-export, trim-extension

--- scripts/zshlang/auto-load/others/rsync.zsh
+++ scripts/zshlang/auto-load/others/rsync.zsh
@@ -9,6 +9,15 @@ function path-parent-dirs {
     awk '{print; while(/\//) {sub("/[^/]*$", ""); print}}'
 }
 ##
+function rsp-notes-export {
+    local rsp_include
+    rsp_include=(${(@f)"$(org-export-recursive "${@}" | trim-extension)"}) @TRET
+
+    ec $'\n\n#####################\n\n'
+
+    reval-ec rsp-notes ~nt/ --rsh="ssh -J walle@193.151.136.67"
+}
+
 function rsp-notes {
     local src="$1"
     src="$(realpath "$src")" @TRET
@@ -18,6 +27,19 @@ function rsp-notes {
     local dest="${rsp_dest:-${lilf_user}@${lilf_ip}:Downloads/static}"
     local pattern="${rsp_pat:-.}"
     assert-args src dest pattern @RET
+
+    ensure-array rsp_include
+    local includes=(${(@)rsp_include})
+    local includes_cmd=()
+    if ((${#includes} >= 1)) ; then
+        includes_cmd=(ugrep -F)
+        local i
+        for i in ${includes[@]} ; do
+            includes_cmd+=(-e "${i}")
+        done
+    else
+        includes_cmd=(cat)
+    fi
 
     local starting_dir="$src"
     if [[ "$src" != */ ]] ; then
@@ -32,16 +54,19 @@ function rsp-notes {
         opts+=(--extension "$ext")
     done
 
+    #: @warning These paths seem to be relative to all included directories. So having 'notes' will cause 'x/notes' to also be included. But I think this will at most cause some empty directories to be copied in our situation, and not files.
     reval-ec fd -uuu --type=file --full-path \
         "${opts[@]}" \
         "$pattern" \
         "$src" |
+        "${includes_cmd[@]}" |
         prefixer -r "$starting_dir" |
         prefixer -r "/" |
         {
             ec "$src"
             path-parent-dirs
         } |
+        duplicates-clean |
         reval-ec rsp-safe --include-from=- --exclude='*' \
             "${rsync_opts[@]}" \
             "$src" "$dest"
