3cb07d270466f258c9a7ac3721522b29c5bc5aa0
NightMachinery <feraidoonmehri@gmail.com>
2022-10-28 09:59:25 +0330

    added curl-remotename

--- scripts/zshlang/auto-load/others/scraping/curl.zsh
+++ scripts/zshlang/auto-load/others/scraping/curl.zsh
@@ -1,3 +1,13 @@
-function gurl() {
+##
+function curl-stdout {
     curlm -o /dev/stdout "$@"
 }
+aliasfn gurl curl-stdout
+##
+function curl-remotename {
+    #: -O, --remote-name          Write output to a file named as the remote file
+    #: -J, --remote-header-name   Use the header-provided filename
+
+    curlm --remote-name --remote-header-name "$@"
+}
+##
