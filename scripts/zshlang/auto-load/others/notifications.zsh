09bc2ad9b6f72174ea0b8f09e614185b77e60074
NightMachinery <feraidoonmehri@gmail.com>
2022-11-28 04:08:39 +0330

    added notif-kitty

--- scripts/zshlang/auto-load/others/notifications.zsh
+++ scripts/zshlang/auto-load/others/notifications.zsh
@@ -1,19 +1,34 @@
-function notif-os() {
+function notif-os {
     local title="$1" msg="$2"
 
     if isServer ; then
         return 0
     fi
-    assert isDarwin @RET
-    if ((${+commands[terminal-notifier]})) ; then
-        terminal-notifier -title "$title" -message "$msg"
+
+    if isDarwin ; then
+        #: @alt `osascript -e 'display notification "msg" with title "hi"'`
+        ##
+        if ((${+commands[terminal-notifier]})) ; then
+            terminal-notifier -title "$title" -message "$msg"
+        else
+            ectrace "terminal-notifier not found"
+            return 1
+        fi
+        ##
     else
-        ectrace "terminal-notifier not found"
-        return 1
+        @NA
+    fi
+}
+
+function notif-kitty {
+    local title="$1" msg="$2"
+
+    if isKitty ; then
+        printf '\x1b]99;i=1:d=0;%s\x1b\\\x1b]99;i=1:d=1:p=body;%s\x1b\\' "$title" "$msg" >/dev/tty
     fi
 }
 ##
-function notif() {
+function notif {
     local msg="$@" tlg="${notif_tlg}"
 
     ec "NOTIF: $msg"
