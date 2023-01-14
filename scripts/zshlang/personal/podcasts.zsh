3ef3cbf09e5fffa6e7ae1a06962b68a08054c073
NightMachinery <feraidoonmehri@gmail.com>
2023-01-14 06:30:19 +0330

    added pdc-opml2zsh-skipexisting, pdc-opml2zsh

--- scripts/zshlang/personal/podcasts.zsh
+++ scripts/zshlang/personal/podcasts.zsh
@@ -16,3 +16,109 @@ podcasts_urls_my['Lex Fridman']='https://lexfridman.com/feed/podcast/'
 podcasts_urls_my['Sharp Tech']='https://sharptech.fm/feed/podcast'
 podcasts_urls_my['80000 Hours']='http://feeds.backtracks.fm/feeds/80000hours/80000-hours-podcast-with-rob-wiblin/feed.xml?1662750094'
 ##
+typeset -Ag podcasts_urls_others
+
+podcasts_urls_others['Talk show with Sima Sabet']=https://www.spreaker.com/show/4153901/episodes/feed
+## imported from OPML:
+podcasts_urls_my['The Robot Brains Podcast']=https://feeds.acast.com/public/shows/the-robot-brains
+podcasts_urls_my['The Thesis Review']=https://feeds.soundcloud.com/users/soundcloud:users:808695991/sounds.rss
+podcasts_urls_my['The Inside View']=https://anchor.fm/s/56df2194/podcast/rss
+podcasts_urls_my['LessWrong Curated Podcast']=https://feeds.buzzsprout.com/2037297.rss
+podcasts_urls_others['AI Governance Fundamentals']=https://audio.beyondwords.io/f/10042/33197/read_8617d3aee53f3ab844a309d37895c143
+podcasts_urls_my['Sharp China with Bill Bishop']=https://sharpchina.fm/feed/podcast
+podcasts_urls_my[Exponent]=https://exponent.fm/feed/
+podcasts_urls_others[Stratechery]=https://rss.stratechery.passport.online/feed/podcast/TLv6iuQ5szMkdGAhY9L3nQ
+podcasts_urls_others['Generally Intelligent']=https://anchor.fm/s/42cab330/podcast/rss
+podcasts_urls_others['The Gradient Podcast']=https://api.substack.com/feed/podcast/265424/s/1354.rss
+podcasts_urls_others['Learning Bayesian Statistics']=https://feeds.captivate.fm/learnbayesstats/
+podcasts_urls_my['TalkRL: The Reinforcement Learning Podcast']=https://feeds.transistor.fm/talkrl
+podcasts_urls_others['DeepMind: The Podcast']=https://feeds.simplecast.com/JT6pbPkg
+podcasts_urls_others['AI with AI: Artificial Intelligence with Andy Ilachinski']=https://aionai.libsyn.com/rss
+podcasts_urls_my['Rationally Speaking Podcast']=https://rationallyspeakingpodcast.libsyn.com/rss
+podcasts_urls_others['Economics Detective Radio']=https://economicsdetective.libsyn.com/rss
+podcasts_urls_my[EconTalk]=https://feeds.simplecast.cmy/wgl4xEgL
+podcasts_urls_others['Planet Money']=https://feeds.npr.org/510289/podcast.xml
+podcasts_urls_others['Signals and Threads']=https://feeds.simplecast.com/L9810DOa
+podcasts_urls_others['The Changelog: Software Development, Open Source']=https://changelog.com/podcast/feed
+podcasts_urls_others['The Talk Show With John Gruber']=https://daringfireball.net/thetalkshow/rss
+podcasts_urls_others['16 Minutes News by a16z']=https://feeds.simplecast.com/j9kKMsfH
+podcasts_urls_others['The TED Interview']=https://feeds.feedburner.com/TedInterview
+podcasts_urls_others[EdgeCast]=https://feeds.fireside.fm/edgecast/rss
+podcasts_urls_my['Twenty Thousand Hertz']=http://feeds.megaphone.fm/20k
+podcasts_urls_others['WSJ Tech News Briefing']=https://video-api.wsj.com/podcast/rss/wsj/tech-news-briefing
+podcasts_urls_my['Freakonomics Radio']=https://feeds.simplecast.com/Y8lFbOT4
+podcasts_urls_others['The Disruptors']=https://disruptors.fm/feed/podcast/
+podcasts_urls_others['The Economist Podcasts']=https://feeds.acast.com/public/shows/ec380acc-fe13-46a0-991f-a1e508d126f8
+podcasts_urls_others['Money Talks from The Economist']=https://feeds.acast.com/public/shows/39fc4a99-8861-437d-81e2-684d13e48f92
+podcasts_urls_others['Editor'\''s Picks from The Economist']=https://feeds.acast.com/public/shows/8457b14f-24d4-4f1a-a274-e6188315170b
+podcasts_urls_others['Future Strategist']=https://feeds.soundcloud.com/users/soundcloud:users:210376229/sounds.rss
+podcasts_urls_others['Conversations with Tyler']=http://cowenconvos.libsyn.com/rss
+podcasts_urls_my['a16z Podcast']=https://feeds.simplecast.com/JGE3yC0V
+podcasts_urls_others['Gadget Lab: Weekly Tech News']=https://feeds.megaphone.fm/gadget-lab
+podcasts_urls_others['99% Invisible']=https://feeds.simplecast.com/BqbsxVfO
+podcasts_urls_others[ManifoldOne]=https://manifoldlearning.com/feed/podcast/
+podcasts_urls_others['a16z Live']=https://feeds.simplecast.com/AuWJKpna
+podcasts_urls_my['AXRP - the AI X-risk Research Podcast']=https://axrpodcast.libsyn.com/rss
+podcasts_urls_others['The CSS Podcast']=https://thecsspodcast.libsyn.com/rss
+podcasts_urls_others['JS Party: JavaScript, CSS, Web Development']=https://changelog.com/jsparty/feed
+podcasts_urls_others['Founders Talk: Startups, CEOs, Leadership']=https://changelog.com/founderstalk/feed
+podcasts_urls_others['Practical AI: Machine Learning, Data Science']=https://changelog.com/practicalai/feed
+podcasts_urls_others['Data Skeptic']=https://dataskeptic.libsyn.com/rss
+podcasts_urls_others['The AI Podcast']=https://feeds.soundcloud.com/users/soundcloud:users:264034133/sounds.rss
+podcasts_urls_others['Machine Learning – Software Engineering Daily']=https://softwareengineeringdaily.com/category/machine-learning/feed/
+podcasts_urls_others['Learning Machines 101']=http://learningmachines101.libsyn.com/rss
+podcasts_urls_others['Gradient Dissent']=https://feeds.captivate.fm/gradient-dissent/
+podcasts_urls_others['Sinocism Podcast']=https://api.substack.com/feed/podcast/2/s/7556.rss
+podcasts_urls_others['Destination Linux']=https://destinationlinux.org/feed/mp3/
+podcasts_urls_others['Unsupervised Learning']=https://www.omnycontent.com/d/playlist/070af456-729b-4a0f-9c09-a6c100397b59/3b159371-276d-429e-ae86-a6c1003b01c4/7b61d4e1-bd3d-4d3f-97c2-a6c1003b01c9/podcast.rss
+podcasts_urls_others['Emacs Chats - Sacha Chua']=https://sachachua.com/blog/tag/emacs-chat/podcast
+podcasts_urls_others['The Freakonomics Radio Book Club']=https://feeds.simplecast.com/KB3PwEoc
+podcasts_urls_others['Clearer Thinking with Spencer Greenberg']=https://clearerthinkingpodcast.com/rss.xml
+podcasts_urls_others['Odd Lots']=https://www.omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/8a94442e-5a74-4fa2-8b8d-ae27003a8d6b/982f5071-765c-403d-969d-ae27003a8d83/podcast.rss
+podcasts_urls_others['The Daily']=https://feeds.simplecast.com/54nAGcIl
+podcasts_urls_my['Making Sense with Sam Harris']=http://wakingup.libsyn.com/rss
+podcasts_urls_others['Making Sense with Sam Harris - Invalid feed']='https://samharris.org/subscriber-rss/?id=5212ri'
+podcasts_urls_others['Secular Jihadists for a Muslim Enlightenment']=https://secularjihadists.libsyn.com/rss
+podcasts_urls_others['Made by Google Podcast']=https://made-by-google-podcast.podigee.io/feed/mp3
+podcasts_urls_others['80k After Hours']=https://feeds.transistor.fm/80k-after-hours
+podcasts_urls_my['Cold Takes Audio']=https://feeds.buzzsprout.com/1851795.rss
+podcasts_urls_others['The Metaculus Journal']=https://feeds.libsyn.com/410111/rss
+podcasts_urls_others['The Nonlinear Library']=https://audio.beyondwords.io/f/8692/7888/read_8617d3aee53f3ab844a309d37895c143
+podcasts_urls_others['The Nonlinear Library: LessWrong Top Posts']=https://spkt.io/f/9196/30270/read_8617d3aee53f3ab844a309d37895c143
+podcasts_urls_others['The Nonlinear Library: EA Forum Top Posts']=https://audio.beyondwords.io/f/9738/32285/read_8617d3aee53f3ab844a309d37895c143
+podcasts_urls_others['The Nonlinear Library: Alignment Forum Top Posts']=https://audio.beyondwords.io/f/9531/31664/read_8617d3aee53f3ab844a309d37895c143
+podcasts_urls_others['The Nonlinear Library: LessWrong']=https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-LW.xml
+podcasts_urls_others['The Nonlinear Library: LessWrong Weekly']=https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-LW-weekly.xml
+podcasts_urls_others['The Nonlinear Library: Alignment Forum']=https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-AF.xml
+podcasts_urls_others['The Nonlinear Library: EA Forum Weekly']=https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-EA-weekly.xml
+podcasts_urls_others['The Data Exchange with Ben Lorica']=https://feeds.buzzsprout.com/682433.rss
+podcasts_urls_others['Chai Time Data Science']=https://anchor.fm/s/c19772c/podcast/rss
+podcasts_urls_others['Microsoft Research Podcast']=https://feeds.blubrry.com/feeds/microsoftresearch.xml
+podcasts_urls_others['Go Time: Golang, Software Engineering']=https://changelog.com/gotime/feed
+podcasts_urls_others['The Jab from The Economist']=https://feeds.acast.com/public/shows/ea536b26-ff86-4e32-870c-f5bbb16461a4
+podcasts_urls_others['Theory and Practice']=https://rss.art19.com/theory-and-practice
+podcasts_urls_others['Big Technology Podcast']=https://feeds.megaphone.fm/LI3617121267
+podcasts_urls_others['Eye On A.I.']=https://aneyeonai.libsyn.com/rss
+podcasts_urls_others['Maths on the Move']=https://feed.podbean.com/plusmathsorg/feed.xml
+podcasts_urls_others['The Filan Cabinet']=https://feeds.libsyn.com/438081/rss
+##
+function pdc-opml2zsh {
+    #: `cpanm String::ShellQuote HTML::Entities`
+    ##
+    perl -nle 'BEGIN{ use String::ShellQuote "shell_quote"; use HTML::Entities ; } ; m/\bxmlUrl="([^"]+)"/ && ($url = shell_quote(decode_entities($1))) && m/\btext="([^"]+)"/ && ($title = shell_quote(decode_entities($1))) && print'" \"podcasts_urls_others[\$title]=\$url\"" |
+        cat-copy-if-tty
+}
+
+function pdc-opml2zsh-skipexisting {
+    local opts=()
+
+    local url
+    for url in "${(@v)podcasts_urls_my}" "${(@v)podcasts_urls_others}" ; do
+        opts+=(-e "$url")
+    done
+
+    pdc-opml2zsh "$@" |
+        ugrep -F -v "${opts[@]}" |
+        cat-copy-if-tty
+}
+##
