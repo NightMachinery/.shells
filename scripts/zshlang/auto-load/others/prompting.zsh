bf1cc840b8d9e46fb3ec7a9c1dc1ddced058868e
NightMachinery <feraidoonmehri@gmail.com>
2023-01-14 06:31:22 +0330

    new prompting.zsh; new tmp.zsh; new date.sh

---
+++ scripts/zshlang/auto-load/others/prompting.zsh
@@ -0,0 +1,19 @@
+##
+function prompt-tex {
+    local input
+    input="$(in-or-args "$@")" @RET
+
+
+    # ec 'Write the following snippet in LaTeX code.'$'\n\n'"$input" |
+    ec 'Write the following snippet using inline LaTeX.'$'\n\n'"$input" |
+        cat-copy-if-tty
+}
+
+function prompt-tex-yc {
+    local input
+    input="$(in-or-args "$@")" @RET
+
+    ec 'Write the following snippet in LaTeX code. Separate different parts of the code with newlines.'$'\n\n'"$input" |
+        cat-copy-if-tty
+}
+##
