c2ed75211b57eebc18648c6c29239104ea7fbf27
NightMachinery <feraidoonmehri@gmail.com>
2023-02-02 03:16:23 +0330

    added img-exif-rm, img-info-exif, img-info-magick

--- scripts/zshlang/auto-load/others/pictures/imagemagick.zsh
+++ scripts/zshlang/auto-load/others/pictures/imagemagick.zsh
@@ -1,4 +1,18 @@
 ##
+function img-info-magick {
+  reval-ec magick identify -verbose "$@"
+}
+
+function img-info-exif {
+  exiftool "$@"
+}
+
+function img-exif-rm {
+  : "Remove all EXIF metadata from the given files"
+
+  exiftool -All= "$@"
+}
+##
 function magick-convert {
   magick convert "$@"
   # @? use =mogrify= instead of =convert= to do it inplace
