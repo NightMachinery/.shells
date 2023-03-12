6fe2ea331a0e95bcb18b831a2679d75b75b7c507
NightMachinery <feraidoonmehri@gmail.com>
2023-03-12 12:21:45 +0330

    .

---
+++ scripts/zshlang/auto-load/others/python_utils/black.zsh
@@ -0,0 +1,17 @@
+##
+function pyblack {
+    command black "$@"
+}
+##
+function pyblack-cat {
+    local tmp
+    tmp="$(gmktemp --suffix=.py)" @TRET
+
+    cat-paste-if-tty > "$tmp" @RET
+
+    reval-ec pyblack "$@" "$tmp" @RET
+
+    cat "$tmp" |
+        cat-copy-if-tty
+}
+##
