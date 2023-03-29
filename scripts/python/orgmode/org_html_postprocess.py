438da28dc03fd32f8c9be7ab78a4ff70401a87b3
NightMachinery <feraidoonmehri@gmail.com>
2023-03-29 18:06:13 +0330

    added openAccessPdf to ss

--- scripts/python/orgmode/org_html_postprocess.py
+++ scripts/python/orgmode/org_html_postprocess.py
@@ -7,34 +7,48 @@ import os
 from icecream import ic
 import re
 from bs4 import BeautifulSoup, NavigableString, XMLParsedAsHTMLWarning
-from IPython import embed
+from pynight.common_ipython import embed_tty
 
 import warnings
-warnings.filterwarnings('ignore', category=XMLParsedAsHTMLWarning)
+
+warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
 html = sys.stdin.read()
-soup = BeautifulSoup(html, features='lxml')
+soup = BeautifulSoup(html, features="lxml")
 
-at_re = re.compile(r'''(?:(?<=\s)|(?<=^))(@(?:\w|/|\d|[][(),.;'])+)(?=\s|$)''')
+at_re = re.compile(
+    r"""(?:(?<=\s)|(?<=^))(?P<tag_raw>@(?P<tag>(?:\w|/|\d|[][(),.;'])+))(?=\s|$)"""
+)
 
-for n in soup.find_all(string=at_re): #: =string= used to be =text=
+
+def replacer(match):
+    tag_raw = match.group("tag_raw")
+    tag = match.group("tag")
+
+    tag = re.sub(r"\W", "_", tag)
+    return f"""<span class="todo at_tag at_{tag}">{tag_raw}</span>"""
+
+
+for n in soup.find_all(string=at_re):  #: =string= used to be =text=
     parent = n.parent
 
     # ic(type(n), n, parent)
-    # with open('/dev/tty') as user_tty:
-    #     sys.stdin=user_tty
-    #     embed()
-    # break
 
-    parent_classes = parent.get('class', []) #: returns a list
+    parent_classes = parent.get("class", [])  #: returns a list
     # ic(parent_classes)
-    if not any(c in parent_classes for c in ('todo', 'done', 'at_tag')):
+
+    if not any(c in parent_classes for c in ("todo", "done", "at_tag")):
         n_str = str(n)
-        n2 = at_re.sub(r'<span class="todo at_tag">\1</span>', n_str)
-        n2_soup = BeautifulSoup(n2,
-                                # features='html.parser',
-                                features='lxml',
-                                )
+
+        # if '@paper' in n_str:
+        #     embed_tty()
+
+        n2 = at_re.sub(replacer, n_str)
+        n2_soup = BeautifulSoup(
+            n2,
+            # features='html.parser',
+            features="lxml",
+        )
         n.replace_with(n2_soup)
 
         # ic(type(n2_soup), n2_soup)
