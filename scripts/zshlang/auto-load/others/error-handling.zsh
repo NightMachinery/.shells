31fcb715d0acd53990fbba5b811cc8a60809f30f
NightMachinery <feraidoonmehri@gmail.com>
2023-05-05 21:08:01 +0330

    retry-limited-eval: added retry_noidle_p

--- scripts/zshlang/auto-load/others/error-handling.zsh
+++ scripts/zshlang/auto-load/others/error-handling.zsh
@@ -7,10 +7,21 @@ function retry-limited() {
 }
 function retry-limited-eval() {
     local retry_sleep="${retry_sleep:-0.5}"
+    local retry_noidle_p="${retry_noidle_p}"
 
     local limit=0
     local ecode=0
-    until {test "$1" -gt 0 && test $limit -ge "$1"} || { eval "${@:2}" && ecode=0 }
+    until {test "$1" -gt 0 && test $limit -ge "$1"} || (
+              if bool "${retry_noidle_p}" ; then
+                  if idle-p ; then
+                      ecerr "$0: skipped retrying because the user was idle"
+
+                      exit 1
+                  fi
+              fi
+
+              eval "${@:2}" && ecode=0
+          )
     do
         ecode="$?"
         ecerr Tried eval "${@:2}" "..."
