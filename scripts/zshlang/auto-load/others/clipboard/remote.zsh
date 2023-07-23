680ed64ff95880210a143fa8528e777bfee7db66
NightMachinery <feraidoonmehri@gmail.com>
2023-07-23 10:22:15 +0330

    added clipboard-remote-listen, pbcopy-remote, bell-call-remote

--- scripts/zshlang/auto-load/others/clipboard/remote.zsh
+++ scripts/zshlang/auto-load/others/clipboard/remote.zsh
@@ -25,3 +25,49 @@ function pbpaste-remote {
 }
 aliasfn popr pbpaste-remote
 ##
+function clipboard-remote-listen {
+    local port="${1:-6030}"
+
+    tmuxnew "clipboard-listen-${port}" \
+        socat -u "TCP-LISTEN:${port},bind=127.0.0.1,fork" 'EXEC:env brishz_in=MAGIC_READ_STDIN brishzq.zsh h-clipboard-remote-listen'
+    # 'EXEC:zsh -fc \"tee >(pbcopy)\"'
+}
+
+function h-clipboard-remote-listen {
+    local isColor_override=y
+
+    local in
+    in="${$(</dev/stdin ; print -n .)[1,-2]}"
+
+    {
+        ec-sep-h
+        ecn "$in"
+        ec-sep-h
+    } >&2
+
+    if [[ "$in" =~ '^MAGIC_BELL_(bell\S?-?\S*)$' ]] ; then
+        bell_name="${match[1]}"
+
+        if isdefined "$bell_name" ; then #: @securityRisk9 @abundanceOfCaution
+            reval "${bell_name}"
+        fi
+    else
+        ecn "$in" |
+            pbcopy
+    fi
+}
+##
+pbcopy-remote() {
+    local port="${copy_port:-6030}"
+
+    socat - "tcp:127.0.0.1:${port}"
+}
+
+bell-call-remote() {
+    local bell_name="${1}"
+    local port="${bell_port:-6030}"
+
+    echo "MAGIC_BELL_${bell_name}" |
+        copy_port="$port" pbcopy-remote
+}
+##
