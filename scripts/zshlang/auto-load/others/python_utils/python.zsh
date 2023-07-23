f8662fec2691f1bd085d49e92ed338d387c89aae
NightMachinery <feraidoonmehri@gmail.com>
2023-07-23 10:25:36 +0330

    added python-package-version

--- scripts/zshlang/auto-load/others/python_utils/python.zsh
+++ scripts/zshlang/auto-load/others/python_utils/python.zsh
@@ -1,4 +1,15 @@
 ##
+function python-package-version {
+    local import_p="${import_p:-y}"
+    if bool "$import_p" ; then
+        import_p=True
+    else
+        import_p=False
+    fi
+
+    python -c "import sys ; import json ; from pynight.common_package import packages_commit_get ; print(json.dumps(packages_commit_get(sys.argv[1:], import_p=${import_p},), indent=2))" "$@"
+}
+##
 function comment-rm-py {
     cat-paste-if-tty |
         perl -CS -lne 'm/^\s*#/ || print' |
