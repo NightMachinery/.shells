3f3b2356ab861d818c2a16779452cd90dd199e2a
NightMachinery <feraidoonmehri@gmail.com>
2023-07-24 20:31:55 +0330

    tsend: ptb supports proxy and sending files, reading config from env

--- scripts/python/telegram-send/tsend.py
+++ scripts/python/telegram-send/tsend.py
@@ -17,15 +17,27 @@ Options:
 Examples:
   tsend.py some_friend "I love you ^_^" --file ~/pics/big_heart.png
 
+Dependencies:
+  pip install -U docopt telethon python-telegram-bot
+
 Created by Fereidoon Mehri. I release my contribution to this program to the public domain (CC0).
 """
 from docopt import docopt
-import os, sys
+import os
+from os import getenv
+import sys
 from pathlib import Path
 import asyncio
-from telethon import TelegramClient, events
 from IPython import embed
 import re
+
+try:
+    from icecream import ic, colorize as ic_colorize
+
+    ic.configureOutput(outputFunction=lambda s: print(ic_colorize(s)))
+except ImportError:
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)
+
 
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
@@ -33,12 +45,23 @@ import re
 
 # os.chdir(os.path.dirname(os.path.realpath(sys.argv[0]))) #Changes pwd to real path, useful for using symlinks for the script.
 # This behavior was disabled because it made sending files inconvenient.
-with open(str(Path.home()) + "/.telegram-config") as f:
-    api_id = int(f.readline())
-    api_hash = f.readline().rstrip()
-    token = f.readline().rstrip()
-    backend = int(f.readline())
-    # print(f"id: {api_id} hash: {api_hash} token: {token} backend: {backend}")
+api_id = getenv("TELEGRAM_API_ID", None)
+api_hash = getenv("TELEGRAM_API_HASH", None)
+token = getenv("TSEND_TOKEN", None)
+backend = getenv("TSEND_BACKEND", None)
+#: backend 2: ptb (python-telegram-bot)
+if backend is not None:
+    backend = int(backend)
+
+# ic(token, backend)
+if not ((backend == 2 and token) or (api_id and api_hash and token and backend)):
+    with open(str(Path.home()) + "/.telegram-config") as f:
+        api_id = int(f.readline())
+        api_hash = f.readline().rstrip()
+        token = f.readline().rstrip()
+        backend = int(f.readline())
+
+# print(f"id: {api_id} hash: {api_hash} token: {token} backend: {backend}")
 
 
 def p2int(p):
@@ -131,15 +154,81 @@ async def discreet_send(
         return last_msg
 
 
+async def ptb_send_file(bot, arguments, max_retries=20):
+    #: @todo read parse mode from arguments
+    ##
+    from telegram.constants import ParseMode
+
+    chat_id = p2int(arguments["<receiver>"])
+    caption = arguments["<message>"]
+
+    for f in arguments["--file"]:
+        for attempt in range(max_retries):
+            is_image = False
+            try:
+                file_type = os.path.splitext(f)[1].lower()  # get file extension
+                is_image = file_type in [".jpg", ".jpeg", ".png"]
+            except:
+                pass
+
+            try:
+                with open(f, "rb") as file:
+                    if is_image:
+                        await bot.send_photo(
+                            chat_id=chat_id,
+                            photo=file,
+                            caption=caption,
+                            parse_mode=ParseMode.MARKDOWN,
+                        )
+                    else:
+                        await bot.send_document(
+                            chat_id=chat_id,
+                            document=file,
+                            caption=caption,
+                            parse_mode=ParseMode.MARKDOWN,
+                        )
+
+                    break
+            except Exception as e:
+                print(f"Error sending {f} (attempt {attempt + 1}): {e}")
+                if attempt == max_retries - 1:  # if it's the last attempt
+                    print(f"Failed to send {f} after {max_retries} attempts.")
+                else:
+                    await asyncio.sleep(
+                        1
+                    )  # sleep for a while before retrying, you can adjust the time as needed
+
+
 async def tsend(arguments):
     arguments["<message>"] = str(arguments["<message>"])
     if backend == 2:
         # print("backend 2 used")
         import telegram
+        from telegram.ext import ApplicationBuilder
 
-        bot = telegram.Bot(token=token)
-        bot.send_message(p2int(arguments["<receiver>"]), arguments["<message>"])
+        proxy_url = os.environ.get("HTTP_PROXY")
+        if proxy_url:
+            app = (
+                ApplicationBuilder()
+                .token(token)
+                .proxy_url(proxy_url)
+                .get_updates_proxy_url(proxy_url)
+                .build()
+            )
+            bot = app.bot
+        else:
+            bot = telegram.Bot(token)
+
+        async with bot:
+            if arguments["--file"]:
+                await ptb_send_file(bot, arguments)
+            else:
+                await bot.send_message(
+                    chat_id=p2int(arguments["<receiver>"]), text=arguments["<message>"]
+                )
     else:
+        from telethon import TelegramClient
+
         # print("telethon used")
         async with TelegramClient(
             str(Path.home()) + "/alice_is_happy", api_id, api_hash
