00c54489b85807c490e32e6992df57852a9dd4bf
NightMachinery <feraidoonmehri@gmail.com>
2023-10-22 18:42:54 +0330

    added battery-charge-limit et al

--- scripts/zshlang/auto-load/others/monitor/battery.zsh
+++ scripts/zshlang/auto-load/others/monitor/battery.zsh
@@ -32,4 +32,65 @@ function power-monitor-darwin {
     sudo powermetrics -i 1000 --poweravg 1 | grep 'Average cumulatively decayed power score' -A 20
 }
 alias energy-monitor-darwin='power-monitor-darwin'
+###
+# Apple Silicon laptops with firmware > 13.0 have a native charge threshold that does not required any userspace daemon running.
+# This native limit works even when the laptop is sleeping or powered off therefore it is preferable to the userspace daemon.
+# Nonetheless, it only works with fixed thresholds (80% as upper limit and 70% as lower limit).
+# CHWA key is the one used to enable/disable the native limit. 01 = 80% limit, 00 = no limit
 ##
+typeset -g smc_command="/usr/local/bin/smc"
+typeset -g smc_charge_limit_key="CHWA"
+typeset -g smc_charge_limit_status_on="01"
+typeset -g smc_charge_limit_status_off="00"
+
+function battery-charge-limit-enable {
+    #: [[id:34b5f3ab-988c-4fdb-93d6-1d0ba6b964f2][battery-charge-limit-enable]]
+    ##
+    if isDarwin ; then
+        assert test -e "${smc_command}" @RET
+
+        reval-ec sudo "${smc_command}" -k "${smc_charge_limit_key}" -w "${smc_charge_limit_status_on}"
+    else
+        @NA
+    fi
+}
+
+function battery-charge-limit-disable {
+    if isDarwin ; then
+        assert test -e "${smc_command}" @RET
+
+        reval-ec sudo "${smc_command}" -k "${smc_charge_limit_key}" -w "${smc_charge_limit_status_off}"
+    else
+        @NA
+    fi
+}
+
+function battery-charge-limit-status {
+    if isDarwin ; then
+        assert test -e "${smc_command}" @RET
+
+        local status_raw="$(reval-ec sudo "${smc_command}" -k "${smc_charge_limit_key}" -r)"
+        # var-show status_raw
+        status_raw="$(ec "$status_raw" | rget '\(bytes (\d+)\)')"
+        # var-show status_raw
+
+        case "$status_raw" in
+            "${smc_charge_limit_status_on}")
+                ec "on"
+                ;;
+            "${smc_charge_limit_status_off}")
+                ec "off"
+                ;;
+            *)
+                ec "$0: unknown ${smc_charge_limit_key} status: $status_raw"
+                ;;
+        esac
+    else
+        @NA
+    fi
+}
+
+aliasfn bls battery-charge-limit-status
+aliasfn ble battery-charge-limit-enable
+aliasfn bld battery-charge-limit-disable
+###
