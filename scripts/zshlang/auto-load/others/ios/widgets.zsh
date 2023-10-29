544c89449bb3bea811684031fc7b28565c10af6b
NightMachinery <feraidoonmehri@gmail.com>
2023-10-29 15:43:24 +0330

    small changes

--- scripts/zshlang/auto-load/others/ios/widgets.zsh
+++ scripts/zshlang/auto-load/others/ios/widgets.zsh
@@ -1,6 +1,8 @@
 ##
 function iwidget-rem {
-    #: @see =rem_comingup_days=
+    #: * @see =rem_comingup_days=
+    #: * @usage
+    #  ** =withremc iwidget-rem=
     ##
     if ! rem-enabled-p ; then
         return 1
@@ -19,5 +21,33 @@ function iwidget-rem-filter {
     local query="$*"
     iwidget-rem |
         rgm --no-context-separator --before-context=1 --after-context=1 "${query}"
+}
+
+function iwidget-rem-oneliner {
+    ec "$(datej-all-short): $(rem-summary-today)"
+}
+
+function iwidget-rem-short {
+    ec "$(datej_all_mode=4 datej-all)"$'\n'"$(rem_comingup_days=0 rem-summary)"
+}
+
+function iwidget-rem-refresh {
+    return 0
+    #: no longer using a cache or a wallpaper
+    ##
+    if [[ "$remindayDir" == "$remindayCDir" ]] ; then
+        ecerr "$0: remindayDir is set to remindayCDir; Skipping refresh."
+        return 0
+    fi
+
+    deus iwidget-rem #: refresh the cache
+
+    if isLocal ; then
+        if bool "$wallpaper_reminder_p" ; then
+            awaysh wallpaper-auto
+        fi
+
+        brishzr awaysh deus iwidget-rem-refresh #: refresh the cache
+    fi
 }
 ##
