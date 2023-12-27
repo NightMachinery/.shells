656b3d5039c7f806c6f5b8166b81867c0b84eeeb
NightMachinery <feraidoonmehri@gmail.com>
2023-12-27 20:21:32 +0330

    added history-all

--- scripts/zshlang/auto-load/others/history.zsh
+++ scripts/zshlang/auto-load/others/history.zsh
@@ -1,4 +1,11 @@
 ##
+function history-all {
+    #: @seeAlso [agfi:hist-last]
+    ##
+    builtin fc -rl 1 |
+        perl -lpe 's/^\s*\S+\s*//g'
+}
+##
 function invocation-save {
     local dest="$1" cmd=("${@[2,-1]}")
     assert-args dest cmd
