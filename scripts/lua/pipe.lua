31eb3ba82e20965a8af3d922ae298ad3a3e0f639
NightMachinery <feraidoonmehri@gmail.com>
2024-01-10 11:06:15 +0330

    added brishzeval2_out

--- scripts/lua/pipe.lua
+++ scripts/lua/pipe.lua
@@ -132,11 +132,21 @@ end
 function brishzevalbg(cmd)
   brishzeval("{ " .. cmd .. " } &>/dev/null &")
 end
+
 function brishzeval2(cmd)
   local my_in = cmd
-  local my_cmd = "bsh.dash" -- susceptible to getting stuck as bsh.dash uses a single session
+  local my_cmd = "/usr/local/bin/bsh.dash" -- susceptible to getting stuck as bsh.dash uses a single session
   local my_args = {} -- no arguments
   local my_status, my_out, my_err = pipe_simple(my_in, my_cmd, table.unpack(my_args))
 
   return my_out .. my_err
+end
+
+function brishzeval2_out(cmd)
+  local my_in = cmd
+  local my_cmd = "/usr/local/bin/bsh.dash" -- susceptible to getting stuck as bsh.dash uses a single session
+  local my_args = {} -- no arguments
+  local my_status, my_out, my_err = pipe_simple(my_in, my_cmd, table.unpack(my_args))
+
+  return my_out
 end
