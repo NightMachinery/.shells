070d562bf4221ee2a07776843253c7efa76af16f
NightMachinery <feraidoonmehri@gmail.com>
2024-02-08 22:45:34 +0330

    added plot-stdin-histogram-py

--- scripts/zshlang/basic/auto-load/tui.zsh
+++ scripts/zshlang/basic/auto-load/tui.zsh
@@ -1,8 +1,9 @@
-function printz-quoted() {
+##
+function printz-quoted {
     printz "$(gq "$@")"
 }
 
-function printz() {
+function printz {
     test -n "$*" && {
         if isI ; then
             print -rz -- "$@ "
@@ -13,16 +14,49 @@ function printz() {
     }
 }
 ##
-function plot-stdin {
-    # https://github.com/seenaburns/stag
+function plot-stdin-datadash {
+    ##
+    #: https://github.com/seenaburns/stag
     ##
     # ttyplot -s 20 -u '%' # mediocre, buggy
     ##
-    datadash --average-line # install: https://github.com/keithknott26/datadash/issues/5
+    datadash --average-line
+    #: @install https://github.com/keithknott26/datadash/issues/5
     ##
-    # feedgnuplot --stream --terminal 'dumb 120,30' --lines # outputs new plots instead of updating the creen.
-    # `gnuplot -e 'set terminal'` for a list of outputs
+}
+aliasfn plot-stdin plot-stdin-datadash
+
+function plot-stdin-gnuplot {
+    feedgnuplot --stream --terminal 'dumb 120,30' --lines
+    #: outputs new plots instead of updating the creen.
+    #: `gnuplot -e 'set terminal'` for a list of outputs
+}
+
+function plot-stdin-asciigraph {
+    asciigraph -r # usable but no stats and wrong height detection. No fixed scale. `goi github.com/guptarohit/asciigraph/cmd/asciigraph`
+}
+
+function plot-stdin-histogram-py {
+    #: * @usage
+    #: ** See [agfi:grade-stats]
     ##
-    ## asciigraph -r # usable but no stats and wrong height detection. No fixed scale. `goi github.com/guptarohit/asciigraph/cmd/asciigraph`
+    histogram.py "$@"
+}
+
+function plot-stdin-histogram-gnuplot {
+    #: @broken
+    #: [[https://gnuplotting.org/tag/histogram/][histogram « Gnuplotting]]
+    ##
+    ecgray 'Use `plot-stdin-histogram-py` instead.'
+
+    gnuplot -e "set term dumb ${COLUMNS}, 17; set autoscale; set boxwidth 0.5; plot '-' with boxes notitle"
+}
+
+function plot-stdin-histogram-octave {
+    #: @broken
+    ##
+    ecgray 'Use `plot-stdin-histogram-py` instead.'
+
+    octave --no-window-system --eval "data = stdin(); hist(data); xlabel('Bins'); ylabel('Frequency'); title('Histogram of Data from stdin'); print -dpng 'histogram.png'"
 }
 ##
