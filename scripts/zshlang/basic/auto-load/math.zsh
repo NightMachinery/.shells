cf9d03db31147fb5cd050db7471e5269450da7b5
NightMachinery <feraidoonmehri@gmail.com>
2024-02-08 22:45:02 +0330

    added num-max, num-std, num-stats, grade-stats

--- scripts/zshlang/basic/auto-load/math.zsh
+++ scripts/zshlang/basic/auto-load/math.zsh
@@ -1,4 +1,41 @@
 ##
+function num-stats {
+    {
+        > >(labeled average) \
+        > >(labeled num-std) \
+        > >(labeled num-max) \
+        > >(labeled num-min)
+     } |
+        cat
+    #: The ending =cat= makes it wait to finish. I don't know why it doesn't wait otherwise.
+}
+
+function grade-stats {
+    ecgray "$0: Removing less than 10 and clamping to 20 for average and STD ..."
+
+    in-or-args "$@" |
+        {
+            > >(
+                plot-stdin-histogram-py --dpi=320 -b=1 - | icat
+                #: A bigger DPI makes the image larger.
+            ) \
+            > >(
+                perl -nle '(print $_ <= 20 ? $_ : 20) if $_ >= 10' |
+                    > >(labeled average) \
+                        > >(labeled num-std) \
+                        > >(ec ">=10 min: $(num-min)")
+            ) \
+                > >(labeled num-max) \
+                > >(
+                ec "<10:"
+                perl -nle 'print $_ if $_ <= 10 and /\d+/' |
+                    prefixer -a '  '
+                ec
+            )  # > >(labeled num-min)
+        } |
+        cat
+}
+
 function num-min {
     #: =bound= from =num-utils=
     ##
@@ -7,6 +44,24 @@ function num-min {
     #: prints an error when there are no numbers in its input
 
     test -n "$res" && ec "$res" || return 1
+}
+
+function num-max {
+    #: =bound= from =num-utils=
+    ##
+    local res
+    res="$(serr command bound)" @RET
+    #: prints an error when there are no numbers in its input
+
+    test -n "$res" && ec "$res" || return 1
+}
+
+function num-std {
+    octave --no-window-system --eval 'disp(std(scanf("%f"), 1))'
+    #: [[https://octave.sourceforge.io/octave/function/std.html][Function Reference: std]]
+    #: The argument =opt= determines the type of normalization to use. Valid values are
+    #: - 0: :: normalize with /N-1/, provides the square root of the best unbiased estimator of the variance [default]
+    #: - 1: :: normalize with /N/, this provides the square root of the second moment around the mean
 }
 
 function in-sum {
