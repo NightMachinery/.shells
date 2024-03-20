44bbbb7c3c13a8a255f9bc8d11a97d7051895412
NightMachinery <feraidoonmehri@gmail.com>
2024-03-20 16:10:22 +0330

    added zip-create, prefix-shared-dir-strip, prefix-shared-dir-get

--- scripts/zshlang/auto-load/others/zip.zsh
+++ scripts/zshlang/auto-load/others/zip.zsh
@@ -1,9 +1,55 @@
+##
 function zir {
     local dest="${$(removeTrailingSlashes "$1"):r}$2".zip
     \rm "$dest" &> /dev/null
     zip -r "$dest" "$1"
 }
 
+function prefix-shared-dir-get {
+    in-or-args "$@" |
+        perl -e '
+        my @paths = <STDIN>;
+        chomp @paths;
+        my $prefix = $paths[0];
+        for my $path (@paths[1..$#paths]) {
+          while (not $path =~ /^\Q$prefix\E/) {
+            $prefix =~ s{/[^/]*$}{};
+          }
+        }
+        print $prefix;
+  ' |
+        cat-copy-if-tty
+}
+
+function prefix-shared-dir-strip {
+    local paths
+    paths="$(in-or-args "$@")" @RET
+
+    local prefix
+    prefix="$(ec "$paths" | prefix-shared-dir-get)" @TRET
+
+    ec "$paths" | perl -e '
+        my $prefix = shift @ARGV;
+        my @paths = <STDIN>;
+        chomp @paths;
+        for my $path (@paths) {
+            $path =~ s{^\Q$prefix\E/*}{};
+            print "$path\n";
+        }
+    ' "$prefix"
+}
+
+function zip-create {
+  local dest="$1" ; shift
+  assert-args dest @RET
+
+  local -a files=("$@")
+
+  local -a pruned_files=("${(@f)$(prefix-shared-dir-strip "${files[@]}")}")
+
+  reval-ecgray zip -r "$dest" ${pruned_files[@]}
+}
+##
 function unzip2dir() {
     local file="$1" y="${unzip2dir_y:-y}"
     file="$(grealpath -e -- "$file")" @TRET
