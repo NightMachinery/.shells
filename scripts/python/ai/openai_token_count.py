2042596fb4310795a304b48c1fcf8ca5f1c0d134
NightMachinery <feraidoonmehri@gmail.com>
2024-07-26 16:34:26 +0330

    openai_token_count.py: caches the tokenizer

--- scripts/python/ai/openai_token_count.py
+++ scripts/python/ai/openai_token_count.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 ##
+import os
 import tiktoken
 import sys
+from pynight.common_files import mkdir
 
+
+##
+HOME = os.environ["HOME"]
+
+tiktoken_cache_dir = f"{HOME}/tmp/tiktoken_cache"
+mkdir(tiktoken_cache_dir)
+
+os.environ["TIKTOKEN_CACHE_DIR"] = tiktoken_cache_dir
+##
 
 def num_tokens_from_message(message, model="gpt-4"):
     encoding = tiktoken.encoding_for_model(model)
