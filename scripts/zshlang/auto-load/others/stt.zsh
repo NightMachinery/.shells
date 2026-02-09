a89d97ccc84f8f95492c0b747ee06ddc7cfd15c4
NightMachinery <feraidoonmehri@gmail.com>
2026-02-09 10:14:48 +0330

    feat(stt): wrap dictated text when LLM windows are focused

--- scripts/zshlang/auto-load/others/stt.zsh
+++ scripts/zshlang/auto-load/others/stt.zsh
@@ -7,3 +7,39 @@ function stt-recordings-rerun {
     stt-recordings-get | fz | inargsf rgeval-env with-g25-maybe llm-stt-file
 }
 ##
+function llm-focus-p {
+    {
+        # kitty-tab-codex-p
+        ##
+        kitty-is-focused && ! kitty-emacs-focused-p
+        #: Since knowing when Codex is active behind tmux and SSH is difficult, let us whitelist when we don't want the STT tag instead! It's pretty much only in emacs.
+    } ||
+        {
+            local front_app_cached
+            front_app_cached="$(frontapp-get)" @RET
+
+            {
+                browser-focus-p &&
+                    browser-current-match-p '^https://(chatgpt\.|(?:aistudio|gemini)\.google\.)'
+            } ||
+                [[ "${front_app_cached}" == 'com.openai.codex' ]]
+        }
+}
+
+function h-stt-filter {
+    local f="$1"
+
+    local end=''
+    if llm-focus-p ; then
+        ##
+        # snippet-stt-coding ''
+        ##
+        # ec 'The following text has been dictated using STT.'
+        ec '```speech-to-text'
+        end=$'\n''```'$'\n'
+    fi
+
+    cat "${f}"
+    ecn "${end}"
+}
+##
