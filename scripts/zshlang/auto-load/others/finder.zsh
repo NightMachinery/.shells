aa9e883440816eb219767a3125cc9c75620d143f
NightMachinery <feraidoonmehri@gmail.com>
2026-02-11 12:25:33 +0330

    finder: add relative symlink helper and use GNU ln in ln2

--- scripts/zshlang/auto-load/others/finder.zsh
+++ scripts/zshlang/auto-load/others/finder.zsh
@@ -6,7 +6,13 @@ function set-finder-icon-size() {
     /usr/libexec/PlistBuddy -c "set StandardViewSettings:IconViewSettings:iconSize ${1:-128}" ~/Library/Preferences/com.apple.finder.plist # This is for Finder itself.
 }
 ##
-function finder-sel-get() {
-    # @darwinonly
+function finder-sel-get {
+    @darwinOnly
+
     finder-selection-get.as
 }
+
+function finder-sel-link-rel-to-here {
+    reval-ecgray pf ln2 . -sr
+}
+##
