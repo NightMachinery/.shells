6cf74027b19b1716957a4d0853cc9aa7b9985a5e
NightMachinery <feraidoonmehri@gmail.com>
2026-04-11 17:33:02 +0330

    modified: android.zsh

--- scripts/zshlang/auto-load/others/android.zsh
+++ scripts/zshlang/auto-load/others/android.zsh
@@ -1,3 +1,4 @@
+##
 function apkdl() {
     local links
     links=( ${(@f)"$(apkdl.py $1)"} )
@@ -5,3 +6,20 @@ function apkdl() {
 }
 reify apkdl
 noglobfn apkdl
+##
+function h-hotspot-1081-to-9081 {
+    reval-ec gost -L socks5://127.0.0.1:9081 -L http://127.0.0.1:9087 -F "socks5://$(router-ip):1081"
+}
+
+function hotspot-1081-to-9081 {
+    tmuxnewsh2 hotspot-1081-to-9081 h-hotspot-1081-to-9081
+}
+
+function 1070-to-9081 {
+    tmuxnewsh2 hotspot-1081-to-9081 gost -L socks5://127.0.0.1:9081 -L http://127.0.0.1:9087 -F "socks5://localhost:1070"
+}
+
+function 1097-to-9081 {
+    tmuxnewsh2 hotspot-1081-to-9081 gost -L socks5://127.0.0.1:9081 -L http://127.0.0.1:9087 -F "socks5://localhost:1097"
+}
+##
