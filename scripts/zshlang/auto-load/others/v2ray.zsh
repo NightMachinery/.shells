32e7924527b87c4892463bf496f54865273dec41
NightMachinery <feraidoonmehri@gmail.com>
2026-05-05 18:16:35 +0330

    feat(v2ray): add xray subscription helpers

--- scripts/zshlang/auto-load/others/v2ray.zsh
+++ scripts/zshlang/auto-load/others/v2ray.zsh
@@ -15,3 +15,25 @@ function xray-json-to-link {
     done | cat-copy-if-tty
 }
 ##
+function xray-sub-get {
+    local url="${1}"
+    assert-args url @RET
+
+    curl --fail --location  --suppress-connect-headers --styled-output --cookie-jar /dev/null "${url}" |
+        gbase64 --decode |
+        cat-copy-if-tty
+}
+
+function xray-sub2json {
+    local links
+    links="$(xray-sub-get "$@")" @RET
+
+    ec "${links}" |
+        Xray-Link-Json - |
+        command jq . |
+        cat-copy-if-tty
+
+    # jqm '.outbounds' |
+    #     perl -ne 'print $p if $. > 2; $p = $_' | head -n -1 |
+}
+##
