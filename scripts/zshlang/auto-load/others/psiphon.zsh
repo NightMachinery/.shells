d63aacfcc0b8a6b7622db1c4981dccb8c0643bc4
NightMachinery <feraidoonmehri@gmail.com>
2026-05-30 12:29:32 +0330

    modified: psiphon.zsh

--- scripts/zshlang/auto-load/others/psiphon.zsh
+++ scripts/zshlang/auto-load/others/psiphon.zsh
@@ -176,13 +176,13 @@ function h-psiphon-builtin-base-config {
   "PropagationChannelId": "FFFFFFFFFFFFFFFF",
   "RemoteServerListSignaturePublicKey": "MIICIDANBgkqhkiG9w0BAQEFAAOCAg0AMIICCAKCAgEAt7Ls+/39r+T6zNW7GiVpJfzq/xvL9SBH5rIFnk0RXYEYavax3WS6HOD35eTAqn8AniOwiH+DOkvgSKF2caqk/y1dfq47Pdymtwzp9ikpB1C5OfAysXzBiwVJlCdajBKvBZDerV1cMvRzCKvKwRmvDmHgphQQ7WfXIGbRbmmk6opMBh3roE42KcotLFtqp0RRwLtcBRNtCdsrVsjiI1Lqz/lH+T61sGjSjQ3CHMuZYSQJZo/KrvzgQXpkaCTdbObxHqb6/+i1qaVOfEsvjoiyzTxJADvSytVtcTjijhPEV6XskJVHE1Zgl+7rATr/pDQkw6DPCNBS1+Y6fy7GstZALQXwEDN/qhQI9kWkHijT8ns+i1vGg00Mk/6J75arLhqcodWsdeG/M/moWgqQAnlZAGVtJI1OgeF5fsPpXu4kctOfuZlGjVZXQNW34aOzm8r8S0eVZitPlbhcPiR4gT/aSMz/wd8lZlzZYsje/Jr8u/YtlwjjreZrGRmG8KMOzukV3lLmMppXFMvl4bxv6YFEmIuTsOhbLTwFgh7KYNjodLj/LsqRVfwz31PgWQFTEPICV7GCvgVlPRxnofqKSjgTWI4mxDhBpVcATvaoBl1L/6WLbFvBsoAUBItWwctO2xalKxF5szhGm8lccoc5MZr8kfE0uxMgsxz4er68iCID+rsCAQM=",
   "RemoteServerListURL": "https://s3.amazonaws.com//psiphon/web/mjr4-p23r-puwl/server_list_compressed",
-  "RemoteServerListURLs": [
-    "https://s3.amazonaws.com//psiphon/web/mjr4-p23r-puwl/server_list_compressed"
-  ],
   "SponsorId": "FFFFFFFFFFFFFFFF",
   "UseIndistinguishableTLS": true
 }
 EOF
+  # "RemoteServerListURLs": [
+  #   "https://s3.amazonaws.com//psiphon/web/mjr4-p23r-puwl/server_list_compressed"
+  # ],
 }
 
 function h-psiphon-write-json-file {
