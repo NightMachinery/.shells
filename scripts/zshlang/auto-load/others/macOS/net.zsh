5587568caf7a3324cb8e2b655ef80d994c496571
NightMachinery <feraidoonmehri@gmail.com>
2026-06-05 09:14:48 +0330

    fix(net): copy router mac when interactive

--- scripts/zshlang/auto-load/others/macOS/net.zsh
+++ scripts/zshlang/auto-load/others/macOS/net.zsh
@@ -4,6 +4,7 @@ function router-mac-darwin {
     gw="$(route -n get default 2>/dev/null | awk '/gateway:/{print $2}')" @TRET
 
     assert silent ping -c1 -t1 "$gw" @RET
-    arp -n "$gw" | awk '{print $4}'
+    arp -n "$gw" | awk '{print $4}' |
+        cat-copy-if-tty
 }
 ##
