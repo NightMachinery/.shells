ddae75454e80c7cd4dbd40a3d14f20a1fbdae42d
NightMachinery <feraidoonmehri@gmail.com>
2026-06-08 14:32:53 +0330

    telegram: add mahfel chat id

--- scripts/zshlang/personal/telegram.zsh
+++ scripts/zshlang/personal/telegram.zsh
@@ -5,6 +5,7 @@ export arista='-1001154785017'
 export water='-1001293952668'
 export ephemeral='-1001404743282'
 export tlogs='-1001460496622'
+export tlg_mahfel='-1003901506504'
 ##
 export tlg_notifs='-1001185370891'
 export tlg_notifc="${tlg_notifs}"
