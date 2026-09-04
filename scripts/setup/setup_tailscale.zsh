c8e56eaaef419887ab6a91301bdb670575b2620d
NightMachinery <feraidoonmehri@gmail.com>
2026-09-04 21:56:38 +0200

    chore(setup): trace tailscale setup

--- scripts/setup/setup_tailscale.zsh
+++ scripts/setup/setup_tailscale.zsh
@@ -3,7 +3,10 @@
 # auth-key material in this public repository.
 
 emulate -L zsh
-setopt err_return no_unset pipe_fail
+export PS4='> '
+setopt LOCAL_OPTIONS PIPE_FAIL PRINT_EXIT_VALUE ERR_RETURN SOURCE_TRACE XTRACE
+setopt TYPESET_SILENT NO_CASE_GLOB multios re_match_pcre extendedglob pipefail interactivecomments hash_executables_only
+setopt NO_BANG_HIST 2>/dev/null || true
 
 function log {
     print -ru2 -- "==> $*"
